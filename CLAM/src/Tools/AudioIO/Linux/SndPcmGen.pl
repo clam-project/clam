d7e0d36a9b65df7fb90a9f25fdafdc06a42bb94f
mdeboer <mdeboer@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-18 10:39:52 +0000

    the removal fix failed. cvs happens.. now the setrate is more robust and allows small Hz offs
    git-svn-id: https://clam-project.org/clam/branches/development-branch@147 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Tools/AudioIO/Linux/SndPcmGen.pl
+++ CLAM/src/Tools/AudioIO/Linux/SndPcmGen.pl
@@ -41,22 +41,22 @@ print HXX<<EOF;
 DON'T EDIT THIS FILE BY HAND, IT IS GENERATED 
 BY THE SCRIPT SndPcmGen.pl FROM
 alsa-lib/test/latency.c
-SndPcm.cxx.in.head
-SndPcm.cxx.in.tail
-SndPcm.hxx.in.head
-SndPcm.hxx.in.tail
+SndPcm.in.head.cxx
+SndPcm.in.tail.cxx
+SndPcm.in.head.hxx
+SndPcm.in.tail.hxx
 */
 
 EOF
 
-open IN,"SndPcm.cxx.in.head";
+open IN,"SndPcm.in.head.cxx";
 while (<IN>)
 {
 	print CXX;
 }
 close IN;
 
-open IN,"SndPcm.hxx.in.head";
+open IN,"SndPcm.in.head.hxx";
 while (<IN>)
 {
 	print HXX;
@@ -104,7 +104,17 @@ while (<LAT>)
 		s/.*\*max.*//;
 		s/exit\(.*\)/return -1;/;
 		s/printf/cat_error/;
+		if (m/snd_pcm_hw_params_set_rate_near/)
+		{
+			print CXX "setrate:\n";
+		}
 		print CXX $_;
+		if (m/err != rate/)
+		{
+			print CXX<<EOF;
+		if (abs(err-rate)<3) { rate = err; goto setrate; }
+EOF
+		}
 	}
 	if (m/^{/ && $inproto)
 	{
@@ -123,14 +133,14 @@ while (<LAT>)
 	}
 }
 
-open IN,"SndPcm.cxx.in.tail";
+open IN,"SndPcm.in.tail.cxx";
 while (<IN>)
 {
 	print CXX;
 }
 close IN;
 
-open IN,"SndPcm.hxx.in.tail";
+open IN,"SndPcm.in.tail.hxx";
 while (<IN>)
 {
 	print HXX;
