e7ad0c6fa65988afdf6b5e2a8054d5ff04147ed3
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-24 10:23:01 +0000

    AnalysisSynthesisExample is now able to play Audio.
    git-svn-id: https://clam-project.org/clam/branches/development-branch@209 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/examples/AnalysisSynthesis/UserInterface.cxx
+++ CLAM/examples/AnalysisSynthesis/UserInterface.cxx
@@ -526,7 +526,7 @@ void UserInterface::cb_mWindow2(Fl_Window* o, void* v) {
 
 UserInterface::UserInterface() {
   Fl_Double_Window* w;
-  { Fl_Double_Window* o = mWindow = new Fl_Double_Window(1028, 746, "SMS Analysis/Synthesis Application | MTG-UPF (Barcelona)");
+  { Fl_Double_Window* o = mWindow = new Fl_Double_Window(1114, 796, "SMS Analysis/Synthesis Application | MTG-UPF (Barcelona)");
     w = o;
     o->color(23);
     o->user_data((void*)(this));
