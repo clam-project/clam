c21cc58085d4be642e81a495e02662de5b8475cc
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-24 09:17:13 +0000

    + floating point rounding to nearest integer mishap fixed
    git-svn-id: https://clam-project.org/clam/branches/development-branch@206 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/GL/GLArrayRenderer.cxx
+++ CLAM/src/Visualization/GL/GLArrayRenderer.cxx
@@ -159,7 +159,7 @@ void GLArrayRenderer::YaxisTransform( TData top, TData bottom, TData& transtop, 
 void GLArrayRenderer::PerformCulling( float left, float right, unsigned pixel_width )
 {
 	mCullingData.left = left*GetXConversionFactor();
-	mCullingData.right = right*GetXConversionFactor();
+	mCullingData.right = (right*GetXConversionFactor())-1;
 	mCullingData.pixel_width = pixel_width;
 	mCullingRequested = true;
 	mMustGenerateIndexes = true;	
