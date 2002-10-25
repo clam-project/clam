137f18ff711f82e05b47b069131c3894a72d6493
mdeboer <mdeboer@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-25 16:16:03 +0000

    removed debug printfs
    git-svn-id: https://clam-project.org/clam/branches/development-branch@243 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Tools/AudioIO/AudioManager.cxx
+++ CLAM/src/Tools/AudioIO/AudioManager.cxx
@@ -65,19 +65,12 @@ AudioManager::AudioManager(int sampleRate,int latency) throw(Err)
 
 AudioManager::~AudioManager()
 {
-	printf(">1AudioManager::~AudioManager\n");
-	printf(">8AudioManager::~AudioManager\n");
 	unsigned int i;
-	printf(">2AudioManager::~AudioManager\n");
 	for (i=0;i<mDevices.size(); i++)
 	{
-		printf(">3AudioManager::~AudioManager\n");
-		printf("deleting %d\n",i);
 		delete mDevices[i];
 	}
-	printf(">4AudioManager::~AudioManager\n");
 	pSingleton = 0;
-	printf("AudioManager::pSingleton=0\n");
 }
 
 AudioDevice* AudioManager::FindDevice(const std::string& name)
