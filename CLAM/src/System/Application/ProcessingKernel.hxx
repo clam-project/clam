f4b28f678118040bbf9abedc4fd6dd63f80c9ead
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-10-31 03:00:06 +0000

    + Visual Poltergeis about functors and pure virtual methods solved (?)
    git-svn-id: https://clam-project.org/clam/branches/development-branch@307 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/System/Application/ProcessingKernel.hxx
+++ CLAM/src/System/Application/ProcessingKernel.hxx
@@ -14,9 +14,23 @@ public:
 
 	virtual ~ProcessingKernel() {}
 
-	virtual void ProcessingCleanup() = 0;
 
-	virtual void ProcessingLoop() = 0;
+	//MRJ: For some reason with Visual Studio 6.0 ( cl version 12.00.8168 )
+	//     is not able to resolve the correct pointer for a derived class
+	//     when accessing to a BaseClass::PureVirtualMethod implemented in
+	//     the derived. Possible culprits:
+	//          + Most surely, since the functor constructor is a virtual one
+	//            through a tricky template function, *IS* possible that during
+	//            some compile-time conversion, the compiler discards qualifiers
+	//            over the reference to the derived class, thus producing a 
+	//            "real pointer" (vcall) which seems to be some kind of compiler
+	//            generated member pointer to identify pure virtual calls. Strange,
+	//            huh?  Nonethless, this works right for patched Micro$oft compilers and
+	//            gcc...
+
+	virtual void ProcessingCleanup(){};
+
+	virtual void ProcessingLoop(){};
 
 	void AttachToThread( Thread& att_thread );
 
