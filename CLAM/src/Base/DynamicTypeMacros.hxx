4fe45601869776e5f20ae8c0db561a3377cc1534
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-11-12 16:40:15 +0000

    + template <unsigned int NAttrib, typename Visitor> \ 	void VisitChainedAttr (AttributePosition<NAttrib>*a, Visitor & visitor) { \ 		CheckAttribute ((AttributePosition<NAttrib>::InboundsCheck*)NULL, \ 		                (AttributePosition<NAttrib>*)NULL); \ 	}\    Commented
    git-svn-id: https://clam-project.org/clam/branches/development-branch@349 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Base/DynamicTypeMacros.hxx
+++ CLAM/src/Base/DynamicTypeMacros.hxx
@@ -119,11 +119,12 @@ private: \
 		a->CompilationError_AttributeNotDefined(); \
 	}\
 	/** Undefined link for the Visit method chain (Visit) */ \
-	template <unsigned int NAttrib, typename Visitor> \
+		/*	template <unsigned int NAttrib, typename Visitor> \
 	void VisitChainedAttr (AttributePosition<NAttrib>*a, Visitor & visitor) { \
 		CheckAttribute ((AttributePosition<NAttrib>::InboundsCheck*)NULL, \
 		                (AttributePosition<NAttrib>*)NULL); \
 	}\
+		*/\
 	/** Undefined link for the Remove method chain (Remove) */ \
 	template <unsigned int NAttrib> \
 	void RemoveChainedAttr (AttributePosition<NAttrib>*a) { \
