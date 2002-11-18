0bcf734488c788395f998c5cc89575d4eabe6698
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-11-18 12:00:02 +0000

    + Typo fixed
    git-svn-id: https://clam-project.org/clam/branches/development-branch@393 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Tools/AudioIO/AudioManager.hxx
+++ CLAM/src/Tools/AudioIO/AudioManager.hxx
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-#ifndef __AudioManager__
+#ifndef __AudioManager___
 #define __AudioManager___
 
 #include <vector>
