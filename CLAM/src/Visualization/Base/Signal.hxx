10738a8e22f52eded22bbbec7a5b02e4a5187dba
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-11-26 19:59:44 +0000

    Virtual destructor for Signal Doxigenified (fewly)
    git-svn-id: https://clam-project.org/clam/branches/development-branch@553 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/Base/Signal.hxx
+++ CLAM/src/Visualization/Base/Signal.hxx
@@ -10,11 +10,23 @@ namespace CLAMGUI
 
 class Connection;
 
+/**
+ * A signal represents an event to be signaled to a group of Slots.
+ *
+ * This class is an abstract one, it only defines the common interface
+ * for signals and some common implementation.
+ */
+
 class Signal
 {
 public:
 	typedef unsigned  tConnectionId;
 
+	virtual ~Signal(){}
+
+	/**
+	 * Undoes the given Conection
+	 */
 	virtual void FreeConnection( Connection* ) = 0;
 
 protected:
@@ -24,9 +36,9 @@ protected:
 	// Types
 	typedef std::stack< unsigned >      tConnectionIdStack;
 protected:
-	
+
 	static tConnectionIdStack  smFreeIdStack;
-    static tConnectionId       smLastConnectionId;
+	static tConnectionId       smLastConnectionId;
 };
 
 }
