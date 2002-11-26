87ab97d5172a7253a0d44a16aa8b2244ac60e709
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-11-26 13:58:08 +0000

    This seems more reliable to me because you don't have to bother other compilers with MS stuff. What about Metrowerks?
    git-svn-id: https://clam-project.org/clam/branches/development-branch@547 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/Base/Signalv1.hxx
+++ CLAM/src/Visualization/Base/Signalv1.hxx
@@ -7,10 +7,10 @@
 
 // MRJ: Argh! I hate this, but I hope at least this is a
 // quite clear way to differentiate both implementations
-#if (_MSC_VER >= 1310)||defined( __GNUC__ )
-#include "Signalv1ImplSerious.hxx"
-#else
+#if defined(_MSC_VER) && (_MSC_VER < 1310)
 #include "Signalv1ImplVC6.hxx"
+#else
+#include "Signalv1ImplSerious.hxx"
 #endif
 
 
