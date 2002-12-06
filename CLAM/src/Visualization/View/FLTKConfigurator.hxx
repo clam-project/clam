6a737fc4a14f5ccad059a3fc3453fa1b061ce0fc
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2002-12-06 11:06:49 +0000

    + "Apply Discard OK" triad button positioning is now a bit less magical    number dependant
    git-svn-id: https://clam-project.org/clam/branches/development-branch@589 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/View/FLTKConfigurator.hxx
+++ CLAM/src/Visualization/View/FLTKConfigurator.hxx
@@ -81,7 +81,12 @@ namespace CLAM{
 
 			GetInfo();
 			
-			Fl_Pack * buttons = new Fl_Pack(w()-3*((w()-40)/3)-10,(mWidgetNum-1)*20,600,20);
+//			Fl_Pack * buttons = new Fl_Pack(w()-3*((w()-40)/3)-10, (mWidgetNum+1)*25,600,20);
+			// MRJ: Buttons should appear as the mWidget+1 widget plus a small offset
+			// MRJ: This should center also properly the three buttons. The idea is to divide
+			// the widget's width into 17 equal parts, and then first position the widget 
+			// left coordinates and adjust widget's width on will
+			Fl_Pack* buttons = new Fl_Pack( (w()/17)*1 , (mWidgetNum+1)*25, (w()/17)*13, 20 );
 			buttons->type(FL_HORIZONTAL);
 			buttons->spacing(4);
 
