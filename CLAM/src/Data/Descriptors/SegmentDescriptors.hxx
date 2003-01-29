298f2f15b772d8ebdad9544311b1427c605f6e77
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-01-29 19:20:39 +0000

    CopyInit and DefaultInit are defined in order to initialize the pointer to descripted data
    git-svn-id: https://clam-project.org/clam/branches/development-branch@754 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Data/Descriptors/SegmentDescriptors.hxx
+++ CLAM/src/Data/Descriptors/SegmentDescriptors.hxx
@@ -70,13 +70,14 @@ class SegmentDescriptors : public ProcessingData {
 
 		SegmentDescriptors(Segment* pSegment);
 
-		void DefaultInit();
-				
-		const Segment* GetpSegment() const{return mpSegment;}
-		void SetpSegment(Segment* pSegment) {mpSegment=pSegment;}
+		void SetpSegment(Segment* pSegment);
+		const Segment* GetpSegment() const;
 		FrameDescriptors& GetFrameD(TIndex pos) {return GetFramesD()[pos];}
 
-		
+	private:
+		void DefaultInit();
+		void CopyInit(const SegmentDescriptors & copied);
+
 	private:
 		Segment* mpSegment;
 
@@ -84,6 +85,26 @@ class SegmentDescriptors : public ProcessingData {
 
 
 };
+
+
+// Implementation
+
+inline void SegmentDescriptors::DefaultInit() {
+	mpSegment=0;
+}
+
+inline void SegmentDescriptors::CopyInit(const SegmentDescriptors & copied) {
+	mpSegment=copied.mpSegment;
+}
+
+inline const Frame* SegmentDescriptors::GetpSegment() const {
+	return mpSegment;
+}
+
+inline void SegmentDescriptors::SetpSegment(Frame* pSegment) {
+	mpSegment=pSegment;
+}
+
 
 
 #endif /* __SegmentDescriptors_H__ */
