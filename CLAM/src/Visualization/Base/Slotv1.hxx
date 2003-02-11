3d810b9ace39b38c824534c5359c1c7cfc37bf06
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-11 11:10:09 +0000

    + The slots
    git-svn-id: https://clam-project.org/clam/branches/experimental-visualization-branch@837 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/src/Visualization/Base/Slotv1.hxx
@@ -0,0 +1,63 @@
+#ifndef __SLOTV1__
+#define __SLOTV1__
+
+#include "Slot.hxx"
+#include "CBL.hxx"
+#include "Assert.hxx"
+
+namespace CLAMGUI
+{
+
+template < typename ParmType1 >
+class Slotv1 : public Slot
+{
+		typedef CBL::Functor1<Parmtype1> WrappedFuncType;
+
+public:
+
+		Slotv1()
+				: mIsInit( false )
+		{
+		}
+
+		virtual ~Slotv1()
+		{
+		}
+
+		template < class RefType, typename PtrMember >
+		void Wrap( RefType thisRef, PtrMember pMember )
+		{
+				mFunctor = CBL::makeFunctor( (CBL::Functor1<ParmType1>*)0, *thisRef, pMember );
+				mIsInit = true;
+		}
+
+		template <  typename PtrMember >
+		void Wrap( PtrMember pMember )
+		{
+				mFunctor = CBL::makeFunctor( (CBL::Functor1<ParmType1>*)0, pMember );
+				mIsInit = true;
+		}
+
+
+		const WrappedFuncType& GetMethod() const
+		{
+				CLAM_ASSERT( mIsInit, "Must be initialized" );
+				return mFunctor;
+		}
+
+		void operator()( ParmType1 parm )
+		{
+				CLAM_ASSERT( mIsInit, "Must be initialized" );
+				mFunctor( parm );
+		}
+
+private:
+
+		WrappedFuncType   mFunctor;
+		bool              mIsInit;
+
+};
+
+}
+
+#endif // SlottedMethod.hxx
