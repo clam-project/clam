27c7d1d3e89c96837f268b69d3312a45d8fdd580
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-13 14:56:37 +0000

    Fix: Eps, Asserts are inverse
    git-svn-id: https://clam-project.org/clam/branches/development-branch@845 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/ArithOps/BinaryAudioOp.hxx
+++ CLAM/src/Processing/ArithOps/BinaryAudioOp.hxx
@@ -91,9 +91,9 @@ namespace CLAM
 
 		void Check(Audio& in1,Audio& in2, Audio& out)
 		{
-			CLAM_ASSERT(in1.GetSize() != in2.GetSize(),
+			CLAM_ASSERT(in1.GetSize() == in2.GetSize(),
 				"BinaryAudioOperation::Do(): Incompatible Input Audio Data Sizes");
-			CLAM_ASSERT(in1.GetSize() != out.GetSize(),
+			CLAM_ASSERT(in1.GetSize() == out.GetSize(),
 				"BinaryAudioOperation::Do(): Incompatible Output Audio Data Size");
 			
 		}
