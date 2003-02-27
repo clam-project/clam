0da3764704a80f58af8899e048b7748294c8d1e7
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-27 11:55:17 +0000

    removed header dependency
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1003 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/testingWithCppUnit/ControlsTests/ControlsTest.cxx
+++ CLAM/test/testingWithCppUnit/ControlsTests/ControlsTest.cxx
@@ -18,7 +18,7 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *
 */
-//#include "ControlsTest.hxx"
+
 #include "InControl.hxx"
 #include "Processing.hxx"
 #include "ProcessingComposite.hxx"
