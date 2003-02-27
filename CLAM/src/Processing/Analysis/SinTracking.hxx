9543a5a0bbac7ad6de81d9901cd64dbee1c7fbe1
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-27 16:05:42 +0000

    indentation
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1014 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/Analysis/SinTracking.hxx
+++ CLAM/src/Processing/Analysis/SinTracking.hxx
@@ -19,22 +19,22 @@ namespace CLAM {
 	class SinTrackingConfig: public ProcessingConfig
 	{
 	public:
-	  DYNAMIC_TYPE_USING_INTERFACE (SinTrackingConfig, 4,ProcessingConfig);
-	  /** Name. */
-	  DYN_ATTRIBUTE (0, public, std::string, Name);
-	  /** Frequency deviation in percentage allowed for a peak to be continued (default: 20). */
-	  DYN_ATTRIBUTE (1, public, TData, Threshold);
-	  /** Maximum number of sines which can be tracked at once (default: 50). */
-	  DYN_ATTRIBUTE (2, public, TSize, nMaxSines);
-	  /**
-	   * Determines if an algorithm specialized for inharmonic input 
-	   * is used or one specialized harmonic input is used. Default: false.
-	   */
-	  DYN_ATTRIBUTE (3, public, bool, IsHarmonic);
+		DYNAMIC_TYPE_USING_INTERFACE (SinTrackingConfig, 4,ProcessingConfig);
+		/** Name. */
+		DYN_ATTRIBUTE (0, public, std::string, Name);
+		/** Frequency deviation in percentage allowed for a peak to be continued (default: 20). */
+		DYN_ATTRIBUTE (1, public, TData, Threshold);
+		/** Maximum number of sines which can be tracked at once (default: 50). */
+		DYN_ATTRIBUTE (2, public, TSize, nMaxSines);
+		/**
+		* Determines if an algorithm specialized for inharmonic input 
+		* is used or one specialized harmonic input is used. Default: false.
+		*/
+		DYN_ATTRIBUTE (3, public, bool, IsHarmonic);
 
-	  void DefaultInit();
-	  void DefaultValues();
-	  ~SinTrackingConfig(){};
+		void DefaultInit();
+		void DefaultValues();
+		~SinTrackingConfig(){};
 	};
 
 
