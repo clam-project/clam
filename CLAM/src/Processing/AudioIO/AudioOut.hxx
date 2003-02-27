010ab8f94ce5203bfc47fd75c05c864b17ae7582
mblaauw <mblaauw@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-27 12:40:31 +0000

    Added some more information to the Do() methods documentation.
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1007 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/AudioIO/AudioOut.hxx
+++ CLAM/src/Processing/AudioIO/AudioOut.hxx
@@ -79,7 +79,13 @@ public:
 	 */
 	bool Do(void);
 
-	/** Non supervised mode of Do function. The object writes in the device attached for the appropiate channel. Values selected to write are provided for the data chunk passed by parameter
+	/**
+	 * Non supervised mode of Do function. The object writes in the device attached for the appropiate channel. 
+	 * Values selected to write are provided for the data chunk passed by parameter. On current implementations 
+	 * Do() will return immediately when not all output channels of the device has been 'filled' yet. If this is 
+	 * last channel to be 'filled', Do() will block, until the device is ready to receive more data. The size of 
+	 * the data chunk passed has restrictions which are dependent on the implementation. Most will require the 
+	 * size to be a power-of-two somewhere in the region of 32 samples to 8192 samples.
 	 * @param data The Audio chunk that we want to pass to the selected Device
 	 * @return true if the method has been executed correctly
 	 */
