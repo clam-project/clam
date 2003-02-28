5cfd1add072d7496ddd591b76fd432decc868f16
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-28 11:44:27 +0000

    Minimal skeleton for a test
    git-svn-id: https://clam-project.org/clam/branches/CLAM-test-example-branch@1085 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/test/testingWithCppUnit/FactoryTest/FactoryTest.cxx
@@ -0,0 +1,20 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+
+namespace CLAMTest
+{
+
+	class FactoryTest;
+	CPPUNIT_TEST_SUITE_REGISTRATION( FactoryTest );
+
+	class FactoryTest : public CppUnit::TestFixture
+	{
+		CPPUNIT_TEST_SUITE( FactoryTest );
+
+		CPPUNIT_TEST_SUITE_END();
+	};
+
+
+
+
+} // namespace CLAMTest
