e1294b88ff5b93361d990897079ac654f0b1dd85
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-02-28 12:01:59 +0000

    Generator -> Oscillator
    git-svn-id: https://clam-project.org/clam/branches/CLAM-test-example-branch@1089 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/testingWithCppUnit/FactoryTest/FactoryTest.cxx
+++ CLAM/test/testingWithCppUnit/FactoryTest/FactoryTest.cxx
@@ -1,20 +1,33 @@
 #include <cppunit/extensions/HelperMacros.h>
+#include <iostream>
+#include <typeinfo>
 
+#include "Oscillator.hxx"
 
 namespace CLAMTest
 {
 
-	class FactoryTest;
-	CPPUNIT_TEST_SUITE_REGISTRATION( FactoryTest );
+class FactoryTest;
+CPPUNIT_TEST_SUITE_REGISTRATION( FactoryTest );
 
-	class FactoryTest : public CppUnit::TestFixture
+class FactoryTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( FactoryTest );
+	CPPUNIT_TEST( testCreateOscillatorReturnsAnOscillator );
+
+	CPPUNIT_TEST_SUITE_END();
+
+private:
+	
+	void testCreateOscillatorReturnsAnOscillator() 
 	{
-		CPPUNIT_TEST_SUITE( FactoryTest );
+	//	std::cout << typeid( 2 );
+	//	std::cout << std::typeid( (CLAM::Oscillator*) 0 );
 
-		CPPUNIT_TEST_SUITE_END();
-	};
+	}
+};
 
-
+	
 
 
 } // namespace CLAMTest
