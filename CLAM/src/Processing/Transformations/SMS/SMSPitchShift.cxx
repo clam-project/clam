426ddf6c47822f7428dc70894f941cf9a957cae2
mblaauw <mblaauw@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-04 12:55:51 +0000

    Removed a temporary test which caused an assertion failure with a sample rate > 40kHz.
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1241 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/Transformations/SMS/SMSPitchShift.cxx
+++ CLAM/src/Processing/Transformations/SMS/SMSPitchShift.cxx
@@ -71,7 +71,7 @@ bool SMSPitchShift::Do(const Frame& in, Frame& out)
 {
 	mSpectralRange=in.GetSinusoidalSpec().GetSpectralRange();
 	mIsHarmonic.DoControl(in.GetFundamental().GetFreq(0));
-	CLAM_ASSERT(mSpectralRange<20000,"Error");//test
+//	CLAM_ASSERT(mSpectralRange<20000,"Error");//test
 	//test
 	TData time=in.GetCenterTime();
 	return Do(in.GetSpectralPeakArray(),in.GetResidualSpec(),out.GetSpectralPeakArray(),out.GetResidualSpec());
