263fb8358bae381b836a950e76ed822b44c4ddd7
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-04 17:13:30 +0000

    seperated FactoryRegistryTests
    git-svn-id: https://clam-project.org/clam/branches/CLAM-test-example-branch@1263 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/testingWithCppUnit/FactoryTest/FactoryTest.cxx
+++ CLAM/test/testingWithCppUnit/FactoryTest/FactoryTest.cxx
@@ -1,6 +1,4 @@
 #include <cppunit/extensions/HelperMacros.h>
-#include <iostream>
-#include <typeinfo>
 #include "cppUnitHelper.hxx"
 
 #include "Oscillator.hxx"
@@ -16,14 +14,6 @@ class FactoryTest : public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE( FactoryTest );
 	CPPUNIT_TEST( testCreateOscillatorReturnsAnOscillator );
-	CPPUNIT_TEST( testRegistryGetCreatorUnsafe_WhenIsEmpty );
-	CPPUNIT_TEST( testRegistryGetCreatorSafe_WhenIsEmpty );
-	CPPUNIT_TEST( testRegistryAskTheWrongKey_WithASingleCreator );
-	CPPUNIT_TEST( testRegistryAskTheCorrectKey_WithASingleCreator );
-	CPPUNIT_TEST( testRegistryAskACorrectKey_WithTwoCreators );
-	CPPUNIT_TEST( testRegistryAddCreator_RepeatedKey );
-	
-
 	CPPUNIT_TEST_SUITE_END();
 
 	
@@ -37,106 +27,6 @@ private:
 		CLAMTEST_ASSERT_EQUAL_RTTYPES( CLAM::Oscillator, *returned ); 
 	}
  
-	void testRegistryGetCreatorUnsafe_WhenIsEmpty()
-	{
-		CLAM::FactoryRegistry reg; // an empty factory register
-		try {
-			reg.GetCreator("bla");
-			CPPUNIT_FAIL( "Assert was expected to happen");
-
-		} catch( CLAM::ErrAssertionFailed& expected ) {
-			CPPUNIT_ASSERT_EQUAL( 
-				std::string("the Factory Registry shouldn't be empty"),
-				std::string( expected.what() ) );
-		}
-	}
-
-	void testRegistryGetCreatorSafe_WhenIsEmpty()
-	{
-		CLAM::FactoryRegistry reg; // an  empty factor y register
-		try {
-			reg.GetCreatorSafe("foo");
-			CPPUNIT_FAIL( "it was expected to catch a CLAM::ErrFactory" );
-		} catch (CLAM::ErrFactory& expected) {
-			CPPUNIT_ASSERT_EQUAL_MESSAGE("In ErrFactory message:", 
-				std::string("GetCreatorSafe invoked on an empty registry"), 
-				std::string( expected.what() ) );
-		}
-	}
-
-	void testRegistryAskTheWrongKey_WithASingleCreator()
-	{
-		CLAM::FactoryRegistry reg;
-		// set up:
-		reg.AddCreator( "Oscillator", CLAM::CreateOscillator );
-		
-		CPPUNIT_ASSERT( NULL==reg.GetCreator("non existent key") );
-	}
-
-	void testRegistryAskTheCorrectKey_WithASingleCreator()
-	{
-		CLAM::FactoryRegistry reg;
-		// set up:
-		CLAM::FactoryRegistry::CreatorMethod inserted;
-		inserted = CLAM::CreateOscillator;
-
-		reg.AddCreator( "Oscillator", inserted);
-
-		CPPUNIT_ASSERT( inserted == reg.GetCreator("Oscillator") ); 
-		CPPUNIT_ASSERT( inserted == reg.GetCreatorSafe("Oscillator") ); 
-	}
-
-	void testRegistryAskACorrectKey_WithTwoCreators()
-	{
-		CLAM::FactoryRegistry reg;
-		// set up
-		reg.AddCreator( "Oscillator", CLAM::CreateOscillator );
-		reg.AddCreator( "AudioAdder", CLAM::CreateAudioAdder );
-
-		CLAM::FactoryRegistry::CreatorMethod oscillatorCreator = CLAM::CreateOscillator;
-		CPPUNIT_ASSERT( oscillatorCreator == reg.GetCreator("Oscillator") );
-		CPPUNIT_ASSERT( oscillatorCreator == reg.GetCreatorSafe("Oscillator") );
-	}
-
-	void testRegistryAskIncorrectKey_WithTwoCreators()
-	{
-		CLAM::FactoryRegistry reg;
-		// set up
-		reg.AddCreator( "Oscillator", CLAM::CreateOscillator );
-		reg.AddCreator( "AudioAdder", CLAM::CreateAudioAdder );
-
-		CLAM::FactoryRegistry::CreatorMethod oscillatorCreator = CLAM::CreateOscillator;
-		CPPUNIT_ASSERT( NULL == reg.GetCreator("Oscillator ") );
-		CPPUNIT_ASSERT( NULL == reg.GetCreatorSafe("incorrect as well") );
-	}
-
-	void testRegistryAddCreator_RepeatedKey()
-	{
-		CLAM::FactoryRegistry reg;
-		reg.AddCreator( "Oscillator", CLAM::CreateOscillator );
-
-		try {
-			reg.AddCreator( "Oscillator", CLAM::CreateAudioAdder );
-			CPPUNIT_FAIL( "Assert expected to happen" );
-
-		} catch (CLAM::ErrAssertionFailed& expected ) {}
-	}
-
-	void testRegistryAddCreatorSafe_RepeatedKey()
-	{
-		CLAM::FactoryRegistry reg;
-		reg.AddCreator( "Oscillator", CLAM::CreateOscillator );
-
-		try {
-			reg.AddCreatorSafe( "Oscillator", CLAM::CreateAudioAdder );
-			CPPUNIT_FAIL( "CLAM::ErrFactory expected" );
-
-		} catch (CLAM::ErrFactory& expected) {
-			CPPUNIT_ASSERT_EQUAL_MESSAGE("In ErrFactory message:", 
-				std::string("FactoryRegistry::AddCreatorSafe(...) a repeated key was passed"), 
-				std::string( expected.what() ) );
-		}
-	}
 
 };
 
