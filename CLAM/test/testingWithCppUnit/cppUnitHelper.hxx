30ae47b42baaf8bb80b7cd4dbbf7113fc93a8c7c
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-04 17:35:01 +0000

    inlined function definition
    git-svn-id: https://clam-project.org/clam/branches/CLAM-test-example-branch@1268 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/testingWithCppUnit/cppUnitHelper.hxx
+++ CLAM/test/testingWithCppUnit/cppUnitHelper.hxx
@@ -36,19 +36,25 @@ namespace CLAMTest
 			for (unsigned i=0; i<n; i++) std::cout << " ";
 		}
 		typedef std::vector<CppUnit::Test *> Tests;
-
+		
 	public:
 		static void printTestNames(const CppUnit::Test* test, const unsigned ind=0)
 		{
-			indent(ind);
+			
 			const CppUnit::TestSuite * suite = dynamic_cast<const CppUnit::TestSuite*>( test );
 			if (suite) {
+				std::cout << std::endl;
+				indent(ind);
 				std::cout << "+ " << suite->getName() << std::endl;
 				Tests::const_iterator it;
 				for (it=suite->getTests().begin(); it!=suite->getTests().end(); it++ )
 					printTestNames(*it, ind+4);
 			} else {
-				std::cout << ". " << test->getName() << std::endl;
+				std::string testName = test->getName();
+				indent(ind);
+				std::cout << ". "
+					<< testName.replace(0,testName.find(".")+1,"")
+					<< std::endl;
 			}
 			if (ind==0) {
 				std::cout << "\n\n";
