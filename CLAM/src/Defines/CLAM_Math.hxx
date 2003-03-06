f00075014c85e974cfc54344458b67a3b42cb1ec
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-06 10:08:28 +0000

    Merged from devel
    git-svn-id: https://clam-project.org/clam/branches/unlabeled-1.3.2.4.2@1291 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Defines/CLAM_Math.hxx
+++ CLAM/src/Defines/CLAM_Math.hxx
@@ -16,4 +16,26 @@
 
 #endif //linux
 
+#if defined _MSC_VER && _MSC_VER < 1310 // MSVC++ 6
+	namespace std
+	{	
+		template < typename T >
+		const T& max( const T& a, const T& b) { 
+			return (a>=b)? a : b;
+		}
+		template < typename T >
+		const T& min(const T& a, const T& b) { 
+			return (a<=b)? a : b;
+		}
+	} // namespace std
+#endif // MSVC++ 6
+
+#ifndef __USE_ISOC99
+inline double  round(double _X)
+        {return (floor(_X+0.5)); }
+inline float  round(float _X)
+        {return (floorf(_X+0.5f)); }
+#endif
+
+
 #endif // CLAM_Math.hxx
