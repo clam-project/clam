70792033ff02d906c5729025422b687f7eb022f2
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-11 14:44:37 +0000

    + Merged from experimental-visualization-branch
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1369 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/src/Visualization/Presentation/FLTK/Fl_SinTracks.hxx
@@ -0,0 +1,47 @@
+#ifndef __FL_SINTRACKS__
+#define __FL_SINTRACKS__
+
+#include <FL/Fl_Window.H>
+#include "SinTracksPresentation.hxx"
+#include "SinTracksRenderingManager.hxx"
+
+namespace CLAMVM
+{
+
+		class Fl_X_Axis;
+		class Fl_Y_Axis;
+		class Fl_ZoomSlider;
+		class Fl_GridLayout;
+		class Fl_Gl_Single_Display;
+
+		class Fl_SinTracks : public SinTracksPresentation, public Fl_Window
+		{
+		private:
+				Fl_GridLayout*            mLayout;
+				Fl_X_Axis*                mXAxis;
+				Fl_Y_Axis*                mYAxis;
+				Fl_ZoomSlider*            mXSlider;
+				Fl_ZoomSlider*            mYSlider;
+				Fl_Gl_Single_Display*     mDisplay;
+				SinTracksRM               mDrawMgr;
+
+		protected:
+				virtual void OnNewPartials( const Array<Partial>&, TIndex ); 
+				
+				virtual void OnNewRange( TData );
+
+				virtual void OnNewDuration( TTime begin, TTime end );
+
+		public:
+
+				Fl_SinTracks( int X, int Y, int W, int H, const char* label = 0 );
+				~Fl_SinTracks();
+
+				virtual void resize( int X, int Y, int W, int H );
+				virtual void Show();
+
+		};
+
+}
+
+#endif // Fl_SinTracks.hxx
