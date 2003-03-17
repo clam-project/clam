f729812ac2a5f8c5815cc989454cff396504fac8
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-17 13:02:02 +0000

    testin.wav testout.wav
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1464 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/examples/FlowControlExamples/NonSupervisedSystem/main.cxx
+++ CLAM/examples/FlowControlExamples/NonSupervisedSystem/main.cxx
@@ -5,13 +5,12 @@ using FlowControlExample::System;
 int main()
 {
 	System s(
-		"snd.wav" /* file in */,
-		"nonsupervisedsystem.wav" /* file out */, 
+		"testin.wav" /* file in */,
+		"testout.wav" /* file out */, 
 		512 /* frame size */, 
-		5000 /* frames to compute*/ );
+		500 /* frames to compute*/ );
 
 	s.ProcessAllIterations();
-//	s.DoProcessings( &System::OscillatorToFileOut );
 
 	return 0;
 }
