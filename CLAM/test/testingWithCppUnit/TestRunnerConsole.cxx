b9f592f7a54419399ed7c3bfd1b379e1b882e1ce
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-19 15:22:03 +0000

    Comments
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1482 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/testingWithCppUnit/TestRunnerConsole.cxx
+++ CLAM/test/testingWithCppUnit/TestRunnerConsole.cxx
@@ -2,18 +2,16 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/TestSuite.h>
 #include <cppunit/ui/text/TestRunner.h>
-//#include <cppunit/TextTestResult.h>
 
 #include "cppUnitHelper.hxx"
 #include "Assert.hxx"
 
 int main(void){
-	CLAM::ErrAssertionFailed::breakpointInCLAMAssertEnabled = false;
 	// this flag allows clam asserts to behave throwing an exception instead
 	// of doing breakpoint. Thus making auto-testing of asserts possible.
 	// Notice: while debugging tests (if breakpoints are wanted back) you might need to
 	// modify this flag before the point to be debugged.
-	
+	CLAM::ErrAssertionFailed::breakpointInCLAMAssertEnabled = false;
 
 	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
 	CppUnit::Test* theTest = registry.makeTest();
