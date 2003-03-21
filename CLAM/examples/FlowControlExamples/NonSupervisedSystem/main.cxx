03dd9f646da1e63168ad25bfb0b07f10f19ee947
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-21 17:12:46 +0000

    namespace before the symbol (instead of using directive)
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1519 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/examples/FlowControlExamples/NonSupervisedSystem/main.cxx
+++ CLAM/examples/FlowControlExamples/NonSupervisedSystem/main.cxx
@@ -1,10 +1,9 @@
 #include "NonSupervisedSystem.hxx"
 
-using FlowControlExample::System;
 
 int main()
 {
-	System s(
+	FlowControlExample::System s(
 		"testin.wav" /* file in */,
 		"testout.wav" /* file out */, 
 		512 /* frame size */, 
