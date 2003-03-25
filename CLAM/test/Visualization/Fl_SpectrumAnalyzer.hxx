e10823b1b92e02bc1ecaf2938c69f7d131b9f714
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-03-25 19:31:02 +0000

    Added a label adapted to the new Vis Mod
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1538 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/Visualization/Fl_SpectrumAnalyzer.hxx
+++ CLAM/test/Visualization/Fl_SpectrumAnalyzer.hxx
@@ -8,19 +8,12 @@
 namespace CLAMVM
 {
 		// forwards declarations
-		class Fl_X_Axis;
-		class Fl_Y_Axis;
-		class Fl_ZoomSlider;
 		class Fl_Gl_Single_Display;
 		class Fl_GridLayout;
 
 		class Fl_SpectrumAnalyzer : public SpectrumPresentation, public Fl_Window
 		{
 		private:
-				Fl_X_Axis*                  mXAxis;
-				Fl_Y_Axis*                  mYAxis;
-				Fl_ZoomSlider*              mXSlider;
-				Fl_ZoomSlider*              mYSlider;
 				Fl_GridLayout*              mLayout;
 				Fl_Gl_Single_Display*       mDisplay;
 				SpectrumRenderingManager    mDrawMgr;
