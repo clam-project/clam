d6de18a73aa16a1c6cb4ed602d724511634d3a98
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-01 14:14:33 +0000

    Added optimizations for conversion routines and ftol.
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1608 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Defines/CLAM_Math.hxx
+++ CLAM/src/Defines/CLAM_Math.hxx
@@ -2,6 +2,71 @@
 #define __CLAM_MATH__
 
 #include <cmath>
+//XA
+#ifdef WIN32
+#ifdef _DEBUG
+#include <float.h>
+#endif
+#endif
+#include "Assert.hxx"
+
+
+//optimized positive integer chopping routine for Windows, is equivalent to int(a) but much more efficient
+inline int Chop(float a) {
+#if defined (_MSC_VER)
+            int i;
+#ifdef _DEBUG
+/**IMPORTANT: if in release mode, you are responsible for changing controlfp.
+	You must do so outside the loop that actually calls the loop */
+			CLAM_ASSERT(a>=0,"Chop function only for positive numbers");
+			unsigned int saved;
+			saved = _controlfp(_RC_CHOP,_MCW_RC);
+#endif //_DEBUG
+            __asm {
+                        fld   a
+						//fadd  half
+                        fistp i
+            }
+#ifdef _DEBUG
+			_controlfp(saved, _MCW_RC);
+#endif //_DEBUG
+            return i;
+#else //not WIN32
+            return int(a); // just hope it's an intrinsic.
+#endif //WIN32
+}
+
+/* optimized roundInt implementation valid only for positive integers*/
+inline int Round(float a)
+{
+#if defined (_MSC_VER)
+	int i;
+	static const float half = 0.5f;
+#ifdef _DEBUG
+/**IMPORTANT: if in release mode, you are responsible for changing controlfp.
+	You must do so outside the loop that actually calls the loop */
+	CLAM_ASSERT(a>=0,"Round function only for positive numbers");
+	unsigned int saved;
+	saved = _controlfp(_RC_CHOP,_MCW_RC);
+#endif //_DEBUG
+    __asm {
+			fld   a
+			fadd  half
+            fistp i
+           }
+#ifdef _DEBUG
+			_controlfp(saved, _MCW_RC);
+#endif //_DEBUG
+            return i;
+#else
+	#ifdef __USE_ISOC99
+		return lrint(a);
+	#else
+		return int(rint(a));
+	#endif
+#endif
+}
+
 
 
 #ifndef linux
@@ -37,5 +102,16 @@ inline float  round(float _X)
         {return (floorf(_X+0.5f)); }
 #endif
 
+
+/** Fast "pow" for converting a logarithmic value into linear value ( assumes a log
+scale factor of 20 ). Warning, float should be TData but includes should then be changed**/
+inline float log2lin( float x )
+{
+
+//	static double magic = 1.0 / (20.0 * log10(exp(1.0)))=0.1151292546497;
+
+	return exp( x * 0.1151292546497 );
+
+}
 
 #endif // CLAM_Math.hxx
