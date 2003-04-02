aca2949d228c43909a32e272ddf97175f8844e48
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-02 17:10:50 +0000

    Templates are in one single hxx file If not, maarten build system get in error
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1632 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Flow/Nodes/StreamBuffer.hxx
+++ CLAM/src/Flow/Nodes/StreamBuffer.hxx
@@ -38,7 +38,7 @@ namespace CLAM {
 	 * <p>
 	 * The utility of this class is two-fold:
 	 *   - It provides very eficient stream possition book-keeping for
-     *     the stream clients. They only need to perform Activate() and
+	 *     the stream clients. They only need to perform Activate() and
 	 *     Leave() operations on the region.
 	 *   - It provides (not so eficient) run time checking in debug
 	 *     mode, which eases the manual implementation of client
@@ -163,8 +163,93 @@ namespace CLAM {
 
 // Template method implementations:
 
-#include "StreamBuffer.cxx"
+#include "Array.hxx"
 
+namespace CLAM {
+
+	template<class D, class B>
+	StreamBuffer<D,B>::StreamBuffer(unsigned int max_window_size)
+		: mRegions(),
+		  mBuffer(max_window_size,mRegions)
+	{}
+
+	template<class D, class B>
+	StreamBuffer<D,B>::StreamBuffer(const StreamBuffer& buf)
+		: mRegions(buf.mRegions),
+		  mBuffer(buf.mBuffer.MaxSize(),mRegions)
+	{}
+
+
+	template<class D, class B>
+	WriteStreamRegion *StreamBuffer<D,B>::NewWriter (unsigned int hop,
+	                                                 unsigned int length)
+	{
+		CLAM_ASSERT(mRegions.Writer() == 0,
+		            "StreamBuffer::NewWirter(): Writer already registered");
+		WriteStreamRegion *writer = new WriteStreamRegion(hop,length);
+		mRegions.SetWriter(writer);
+		return writer;
+	}
+
+	template<class D, class B>
+	ReadStreamRegion *StreamBuffer<D,B>::NewReader (unsigned int hop,
+	                                                unsigned int length,
+	                                                SourceStreamRegion* source)
+	{
+		CLAM_ASSERT(source?mRegions.Contains(source):true,
+		            "StreamBuffer::NewReader(): Invalid source argument.");
+		if (!source)
+			source = mRegions.Writer();
+ 		ReadStreamRegion *reader = new ReadStreamRegion(hop,length,source);
+		mRegions.AddReader(reader,source);
+		return reader;
+	}
+
+	template<class D, class B>
+	DelayStreamRegion *StreamBuffer<D,B>::NewDelay  (unsigned int hop,
+	                                                 unsigned int length,
+	                                                 SourceStreamRegion* source = 0)
+	{
+		CLAM_ASSERT(source?mRegions.Contains(source):true,
+		            "StreamBuffer::NewDelay(): Invalid source argument.");
+		if (!source)
+			source = mRegions.Writer();
+ 		DelayStreamRegion *delay = new DelayStreamRegion(hop,length,source);
+		mRegions.AddDelay(delay,source);
+		return delay;
+	}
+
+	template<class D, class B>
+	InplaceStreamRegion *StreamBuffer<D,B>::NewInplace(unsigned int hop, 
+	                                                   unsigned int read_length,
+	                                                   unsigned int write_length,
+	                                                   SourceStreamRegion* source,
+	                                                   unsigned int write_delay)
+	{
+		CLAM_ASSERT(source?mRegions.Contains(source):true,
+		            "StreamBuffer::NewInplace(): Invalid source argument.");
+		if (!source)
+			source = mRegions.Writer();
+ 		InplaceStreamRegion *inplace = new InplaceStreamRegion(hop,length,source);
+		mRegions.AddInplace(inplace,source);
+		return inplace;
+	}
+
+	template<class D, class B>
+	void StreamBuffer<D,B>::Configure(unsigned int max_window_size)
+	{
+		CLAM_ASSERT(mRegions.FulfilsInvariant(),
+		            "StreamBuffer::Configure(): "
+		            "Inconsisntent regions before configuration")
+
+		mBuffer.Configure(max_window_size);
+
+		CLAM_ASSERT(mBuffer.FulfilsInvariant(),
+		            "StreamBuffer::Configure(): "
+		            "Inconsistent buffer after configuration.");
+	}
+
+}
 // Class speciallizations:
 
 #include "AudioStreamBuffer.hxx"
