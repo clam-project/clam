7ff6b359235799bff96bd91c05794cf26de3975d
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-04 12:43:52 +0000

    + First commited to CVS
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1678 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/src/Visualization/Presentation/Base/InControlPresentation.cxx
@@ -0,0 +1,27 @@
+#include "InControlPresentation.hxx"
+
+namespace CLAMVM
+{
+		InControlPresentation::InControlPresentation()
+		{
+				ReceiveValue.Wrap( this, &InControlPresentation::OnNewValue );
+				ReceiveValueRange.Wrap( this, &InControlPresentation::OnNewRange );
+		}
+
+		InControlPresentation::~InControlPresentation()
+		{
+		}
+
+		void InControlPresentation::AttachTo( InputControlModel& model )
+		{
+				SendValue.Connect( model.UpdateValue );
+				model.ValuePublished.Connect( ReceiveValue );
+				model.ValueRangePublished.Connect( ReceiveValueRange );
+		}
+
+		void InControlPresentation::Detach()
+		{
+				ReceiveValue.Unbind();
+				ReceiveValueRange.Unbind();
+		}
+}
