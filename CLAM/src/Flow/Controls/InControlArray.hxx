7e5b4f7631519e3105576a2760c45402419a2449
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-14 11:01:07 +0000

    Array -> std::vector
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1777 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Flow/Controls/InControlArray.hxx
+++ CLAM/src/Flow/Controls/InControlArray.hxx
@@ -2,7 +2,7 @@
 #define _InControlArray_hxx_
 
 #include "InControl.hxx"
-//#include <vector>
+#include <vector>
 
 namespace CLAM
 {
@@ -12,7 +12,8 @@ namespace CLAM
  */ 
 class InControlArray
 {
-	Array<InControl*> mArray;
+	typedef std::vector<InControl*> Controls;
+	Controls mControls;
 public:
 
 	inline InControlArray(
@@ -22,8 +23,8 @@ public:
 
 	inline ~InControlArray();
 
-	inline InControl       &operator[](int i)        { return *mArray[i]; }
-	inline const InControl &operator[](int i) const  { return *mArray[i]; }
+	inline InControl       &operator[](int i)        { return *mControls.at(i); }
+	inline const InControl &operator[](int i) const  { return *mControls.at(i); }
 };
 
 
@@ -31,24 +32,21 @@ InControlArray::InControlArray(int size,
                                const std::string &name,
                                Processing *parent /*0 by default. See decl above*/)
 {
-	mArray.Resize(size);
-	mArray.SetSize(size);
+	mControls.resize(size);
 	for (int i=0; i<size; i++) {
 		std::stringstream str;
-		str.str("");
 		str << name << "_" << i;
 		if (parent)
-			mArray[i] = new InControl(str.str(),parent);
+			mControls.at(i) = new InControl(str.str(),parent);
 		else
-			mArray[i] = new InControl(str.str());
+			mControls.at(i) = new InControl(str.str());
 	}
 }
 
 InControlArray::~InControlArray()
 {
-	int size = mArray.Size();
-	for (int i=0; i<size; i++)
-		delete mArray[i];
+	for (std::size_t i=0; i<mControls.size(); i++)
+		delete mControls[i];
 }
 
 } //namespace CLAM
--- CLAM/src/Flow/Controls/InControlTmplArray.hxx
+++ CLAM/src/Flow/Controls/InControlTmplArray.hxx
@@ -2,7 +2,7 @@
 #define _InControlTmplArray_hxx_
 
 #include "InControl.hxx"
-//#include <vector>
+#include <vector>
 
 namespace CLAM
 {
@@ -14,16 +14,16 @@ class InControlTmplArray
 {
 	typedef InControlTmpl<TProcessing> TInControl;
 	typedef typename TInControl::TPtrMemberFuncId TPtrMemberFuncId;
-
-	Array<TInControl*> mArray;
+	typedef std::vector<TInControl*> Controls;
+	Controls mControls;
 
 public:
 	InControlTmplArray(int size, const std::string &name, TProcessing* parent,
 		TPtrMemberFuncId f, const bool publish=true);
 	~InControlTmplArray();
 
-	inline TInControl& operator[](int i) { return *mArray[i]; }
-	inline const TInControl& operator[](int i) const { return *mArray[i]; }
+	inline TInControl& operator[](int i) { return *mControls.at(i); }
+	inline const TInControl& operator[](int i) const { return *mControls.at(i); }
 
 };
 
@@ -37,21 +37,19 @@ InControlTmplArray<TProcessing>::InControlTmplArray(
 		TPtrMemberFuncId f,
 		const bool publish )
 {
-	mArray.Resize(size);
-	mArray.SetSize(size);
+	mControls.resize(size);
 	for (int i=0; i<size; i++) {
 		std::stringstream str;
 		str << name << "_" << i;
 		CLAM_ASSERT(parent, "InControlTmplArray must be published. Check ctr processing* parameter");
-		mArray[i] = new TInControl(i, str.str(), parent, f);	
+		mControls.at(i) = new TInControl(i, str.str(), parent, f);	
 	}
 }
 template <class TProcessing>
 InControlTmplArray<TProcessing>::~InControlTmplArray()
 {
-	int size = mArray.Size();
-	for (int i=0; i<size; i++)
-		delete mArray[i];
+	for (int i=0; i<mControls.size(); i++)
+		delete mControls.at(i);
 }
 
 } //namespace CLAM
