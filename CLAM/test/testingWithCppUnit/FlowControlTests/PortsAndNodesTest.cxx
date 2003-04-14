47b628cd6e2a77cfa01ad0192a5e49260edf97e2
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-14 11:01:59 +0000

    new test
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1778 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/test/testingWithCppUnit/FlowControlTests/PortsAndNodesTest.cxx
@@ -0,0 +1,86 @@
+/*
+* Copyright (c) 2001-2002 MUSIC TECHNOLOGY GROUP (MTG)
+*                         UNIVERSITAT POMPEU FABRA
+*
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+
+#include <cppunit/extensions/HelperMacros.h>
+//#include "cppUnitHelper.hxx" // needed for assertion_traits<bool>
+#include "Port.hxx"
+#include "Processing.hxx"
+
+
+namespace CLAMTest {
+
+class PortsAndNodesTest;
+CPPUNIT_TEST_SUITE_REGISTRATION( PortsAndNodesTest );
+
+
+class PortsAndNodesTest : public CppUnit::TestFixture, public CLAM::Processing
+{
+	CPPUNIT_TEST_SUITE( PortsAndNodesTest );
+	// Tests for the Port base interface and Node:
+	
+	
+	CPPUNIT_TEST( testAttachInPortToNode_WithGeneralTemplateInPort_GetsAttachedToConcreteNode );
+	CPPUNIT_TEST( testAttachPortsAndGetData_ReadsTheWrittenData );
+
+/*	
+	CPPUNIT_TEST( testInPort_Attach_WithGeneralTemplateInPort_BadTypeAssertionFails );
+	CPPUNIT_TEST( testInPort_IsAttached_WithGeneralTemplateInPort_AfterConstruction );
+	CPPUNIT_TEST( testInPort_IsAttached_WithGeneralTemplateInPort_AfterAttach );
+	CPPUNIT_TEST( testInPort_IsAttached_WithGeneralTemplateInPort_AfterUnattach );
+	CPPUNIT_TEST( testInPort_UnAttach_WithGeneralTemplateInPort_WhenIsNotAttached );
+	
+	//OutPorts
+	CPPUNIT_TEST( testOutPort_Attach_WithGeneralTemplateOutPort_UpdatesConcretePortState );
+	CPPUNIT_TEST( testOutPort_Attach_WithGeneralTemplateOutPort_BadTypeAssertionFails );
+	CPPUNIT_TEST( testOutPort_IsAttached_WithGeneralTemplateOutPort_AfterConstruction );
+	CPPUNIT_TEST( testOutPort_IsAttached_WithGeneralTemplateOutPort_AfterAttach );
+	CPPUNIT_TEST( testOutPort_IsAttached_WithGeneralTemplateOutPort_AfterUnattach );
+	CPPUNIT_TEST( testOutPort_UnAttach_WithGeneralTemplateOutPort_WhenIsNotAttached );
+*/	
+
+	// Tests for the concrete PortTmpl classes
+	CPPUNIT_TEST_SUITE_END();
+
+	// Testing pattern: Self Shunt
+	// Processing interface:
+	const char* GetClassName() const { return "for testing"; }
+	bool Do() { return false; }
+	const CLAM::ProcessingConfig& GetConfig() const { throw 0; }
+	bool ConcreteConfigure( const CLAM::ProcessingConfig& ) { return false; }
+	
+
+	void testAttachInPortToNode_WithGeneralTemplateInPort_GetsAttachedToConcreteNode
+	{
+		CPPUNIT_ASSERT_EQUAL();
+	}
+	/*
+	void testAttachPortsAndGetData_ReadsTheWrittenData()
+	{
+		CLAM::InPortTmpl<DummyProcessingData> concreteInPort
+		CLAM::InPort& in = concreteInPort;
+		DummyProcessingData& returned = in.GetData();
+		CPPUNIT_ASSERT_EQUAL( 1, returned.GetState() );
+	}
+	*/
+
+};
+
+} // namespace
