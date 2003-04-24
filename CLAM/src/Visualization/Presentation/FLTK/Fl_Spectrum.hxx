5d825a8af38f2b11aa6312c51192816ea67f7641
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-24 11:22:25 +0000

    + Tabulation changed
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1899 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/Presentation/FLTK/Fl_Spectrum.hxx
+++ CLAM/src/Visualization/Presentation/FLTK/Fl_Spectrum.hxx
@@ -7,36 +7,36 @@
 
 namespace CLAMVM
 {
-		// forwards declarations
-		class Fl_X_Axis;
-		class Fl_Y_Axis;
-		class Fl_ZoomSlider;
-		class Fl_Gl_Single_Display;
-		class Fl_GridLayout;
+	// forwards declarations
+	class Fl_X_Axis;
+	class Fl_Y_Axis;
+	class Fl_ZoomSlider;
+	class Fl_Gl_Single_Display;
+	class Fl_GridLayout;
 
-		class Fl_Spectrum : public SpectrumPresentation, public Fl_Window
-		{
-		private:
-				Fl_X_Axis*                  mXAxis;
-				Fl_Y_Axis*                  mYAxis;
-				Fl_ZoomSlider*              mXSlider;
-				Fl_ZoomSlider*              mYSlider;
-				Fl_GridLayout*              mLayout;
-				Fl_Gl_Single_Display*       mDisplay;
-				SpectrumRenderingManager    mDrawMgr;
+	class Fl_Spectrum : public SpectrumPresentation, public Fl_Window
+	{
+	private:
+		Fl_X_Axis*                  mXAxis;
+		Fl_Y_Axis*                  mYAxis;
+		Fl_ZoomSlider*              mXSlider;
+		Fl_ZoomSlider*              mYSlider;
+		Fl_GridLayout*              mLayout;
+		Fl_Gl_Single_Display*       mDisplay;
+		SpectrumRenderingManager    mDrawMgr;
 
-		protected:
+	protected:
 				
-				virtual void OnNewSpectrum( const DataArray&, TData );
+		virtual void OnNewSpectrum( const DataArray&, TData );
 
-		public:
-				Fl_Spectrum( int X, int Y, int W, int H, const char* label = 0 );
-				~Fl_Spectrum();
+	public:
+		Fl_Spectrum( int X, int Y, int W, int H, const char* label = 0 );
+		~Fl_Spectrum();
 
-				void resize( int X, int Y, int W, int H );
-				void Show();
-				void Hide();
-		};
+		void resize( int X, int Y, int W, int H );
+		void Show();
+		void Hide();
+	};
 }
 
 #endif // Fl_Spectrum.hxx
