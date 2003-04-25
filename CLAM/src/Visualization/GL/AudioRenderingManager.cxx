0ab9e25c75d81edfdd07d183616177698bb4e215
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-25 07:30:34 +0000

    + Tabulation changed
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1908 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/GL/AudioRenderingManager.cxx
+++ CLAM/src/Visualization/GL/AudioRenderingManager.cxx
@@ -9,164 +9,164 @@
 
 namespace CLAMVM
 {
-		AudioRenderingManager::AudioRenderingManager()
-				: mActiveRenderer( NULL ), mDetailThreshold( 100 ), mMustProcessData( false ), mHugeArrayCondition( false ), mOffset(0)
+	AudioRenderingManager::AudioRenderingManager()
+		: mActiveRenderer( NULL ), mDetailThreshold( 100 ), mMustProcessData( false ), mHugeArrayCondition( false ), mOffset(0)
+	{
+	}
+
+	AudioRenderingManager::~AudioRenderingManager()
+	{
+		if ( mActiveRenderer!=NULL )
+			delete mActiveRenderer;
+	}
+
+	void AudioRenderingManager::SetWorldBounds( const DataBoundBox& dataBBox )
+	{
+		mDataBBox = dataBBox;
+		mMustProcessData = true;
+	}
+
+	void AudioRenderingManager::SetPixelBounds( const ScreenBoundBox& scrBBox )
+	{
+		mScreenBBox = scrBBox;
+
+		mMustProcessData = true;
+	}
+
+	void AudioRenderingManager::CacheData( const DataArray& data, const tAudioTimeInfo& timenfo )
+	{
+		mDataCached.Resize( data.Size() );
+		mDataCached.SetSize( data.Size() );
+
+		std::copy( data.GetPtr(), data.GetPtr()+data.Size(), mDataCached.GetPtr() );
+		mCachedTimeInfo = timenfo;
+
+		mMustProcessData = true;
+	}
+
+	void AudioRenderingManager::ActivateStraightRenderer(  )
+	{
+		if ( mActiveRenderer != NULL )
 		{
+			delete mActiveRenderer;
+			mActiveRenderer = new GLStraightLineArray;
 		}
+		else
+			mActiveRenderer = new GLStraightLineArray;
+	}
 
-		AudioRenderingManager::~AudioRenderingManager()
+	void AudioRenderingManager::ActivateDetailRenderer()
+	{
+		if ( mActiveRenderer != NULL )
 		{
-				if ( mActiveRenderer!=NULL )
-						delete mActiveRenderer;
+			delete mActiveRenderer;
+			mActiveRenderer = new GLSampleDetailArray;
 		}
-
-		void AudioRenderingManager::SetWorldBounds( const DataBoundBox& dataBBox )
-		{
-				mDataBBox = dataBBox;
-				mMustProcessData = true;
-		}
-
-		void AudioRenderingManager::SetPixelBounds( const ScreenBoundBox& scrBBox )
-		{
-				mScreenBBox = scrBBox;
-
-				mMustProcessData = true;
-		}
-
-		void AudioRenderingManager::CacheData( const DataArray& data, const tAudioTimeInfo& timenfo )
-		{
-				mDataCached.Resize( data.Size() );
-				mDataCached.SetSize( data.Size() );
-
-				std::copy( data.GetPtr(), data.GetPtr()+data.Size(), mDataCached.GetPtr() );
-				mCachedTimeInfo = timenfo;
-
-				mMustProcessData = true;
-		}
-
-		void AudioRenderingManager::ActivateStraightRenderer(  )
-		{
-				if ( mActiveRenderer != NULL )
-				{
-						delete mActiveRenderer;
-						mActiveRenderer = new GLStraightLineArray;
-				}
-				else
-						mActiveRenderer = new GLStraightLineArray;
-		}
-
-		void AudioRenderingManager::ActivateDetailRenderer()
-		{
-				if ( mActiveRenderer != NULL )
-				{
-						delete mActiveRenderer;
-						mActiveRenderer = new GLSampleDetailArray;
-				}
-				else
-						mActiveRenderer = new GLSampleDetailArray;
+		else
+			mActiveRenderer = new GLSampleDetailArray;
 				
-		}
+	}
 
-		void AudioRenderingManager::ProcessData()
+	void AudioRenderingManager::ProcessData()
+	{
+		DetermineVisibleSamples();
+			
+		if ( mHugeArrayCondition )
 		{
-			DetermineVisibleSamples();
-			
-			if ( mHugeArrayCondition )
-			{
-					mHugeArrayRenderer.SetArrays( mMaxs.GetPtr(), mMins.GetPtr(), mMaxs.Size() );					
-					return;
-			}
-			
-			if ( mProcessedData.Size() <= mDetailThreshold )
-			{
-					ActivateDetailRenderer();
-			}
-			else
-					ActivateStraightRenderer();
-			
-			mActiveRenderer->SetStartAndHopSize( mOffset, 1 );
-			mActiveRenderer->SetDataPtr( mProcessedData.GetPtr(), mProcessedData.Size() );
-			mMustProcessData = false;
-
+			mHugeArrayRenderer.SetArrays( mMaxs.GetPtr(), mMins.GetPtr(), mMaxs.Size() );					
+			return;
 		}
-
-		void AudioRenderingManager::RenderData()
+			
+		if ( mProcessedData.Size() <= mDetailThreshold )
 		{
-				if ( mMustProcessData )
-				{
-						ProcessData();
-				}
+			ActivateDetailRenderer();
+		}
+		else
+			ActivateStraightRenderer();
+			
+		mActiveRenderer->SetStartAndHopSize( mOffset, 1 );
+		mActiveRenderer->SetDataPtr( mProcessedData.GetPtr(), mProcessedData.Size() );
+		mMustProcessData = false;
+
+	}
+
+	void AudioRenderingManager::RenderData()
+	{
+		if ( mMustProcessData )
+		{
+			ProcessData();
+		}
 				
-				if ( mHugeArrayCondition )
-				{
-						mHugeArrayRenderer.YChanged( mDataBBox.mTop, mDataBBox.mBottom );
-						mHugeArrayRenderer.ExecuteGLCommands();
-				}
-				else
-						mActiveRenderer->ExecuteGLCommands();
-		}
-
-		void AudioRenderingManager::DetermineVisibleSamples()
+		if ( mHugeArrayCondition )
 		{
-				TSize offset, len;
+			mHugeArrayRenderer.YChanged( mDataBBox.mTop, mDataBBox.mBottom );
+			mHugeArrayRenderer.ExecuteGLCommands();
+		}
+		else
+			mActiveRenderer->ExecuteGLCommands();
+	}
 
-				offset = mDataBBox.mLeft;
-				len = (mDataBBox.mRight - mDataBBox.mLeft)+1;
+	void AudioRenderingManager::DetermineVisibleSamples()
+	{
+		TSize offset, len;
 
-				mOffset = offset;
+		offset = mDataBBox.mLeft;
+		len = (mDataBBox.mRight - mDataBBox.mLeft)+1;
 
-				if ( len/5 >= mScreenBBox.pixel_width )
-				{
-						mHugeArrayCondition = true;
-						BuildMaxMinArrays( offset, len );
-						return;
-				}
+		mOffset = offset;
+
+		if ( len/5 >= mScreenBBox.pixel_width )
+		{
+			mHugeArrayCondition = true;
+			BuildMaxMinArrays( offset, len );
+			return;
+		}
 				
-				mHugeArrayCondition = false;
+		mHugeArrayCondition = false;
 						
-				if ( mProcessedData.Size() <= len )
-						mProcessedData.Resize( len+1 );
+		if ( mProcessedData.Size() <= len )
+			mProcessedData.Resize( len+1 );
 
-				mProcessedData.SetSize( len+1 );
+		mProcessedData.SetSize( len+1 );
 
-				std::copy( mDataCached.GetPtr()+offset, mDataCached.GetPtr()+offset+len+1, mProcessedData.GetPtr() );
-		}
+		std::copy( mDataCached.GetPtr()+offset, mDataCached.GetPtr()+offset+len+1, mProcessedData.GetPtr() );
+	}
 
-		void AudioRenderingManager::BuildMaxMinArrays( TSize offset, TSize len )
-		{
-				TSize startSearch, endSearch, searchIntervalLen, searchRemIntervalLen;
-				TSize nMaxs = mScreenBBox.pixel_width;
+	void AudioRenderingManager::BuildMaxMinArrays( TSize offset, TSize len )
+	{
+		TSize startSearch, endSearch, searchIntervalLen, searchRemIntervalLen;
+		TSize nMaxs = mScreenBBox.pixel_width;
 
-				if ( mMaxs.Size() < nMaxs )
-						mMaxs.Resize( nMaxs );
-				if ( mMins.Size() < nMaxs )						
-						mMins.Resize( nMaxs );
+		if ( mMaxs.Size() < nMaxs )
+			mMaxs.Resize( nMaxs );
+		if ( mMins.Size() < nMaxs )						
+			mMins.Resize( nMaxs );
 
-				mMaxs.SetSize( nMaxs );
+		mMaxs.SetSize( nMaxs );
 
-				mMins.SetSize( nMaxs );
+		mMins.SetSize( nMaxs );
 
-				searchIntervalLen = len / (nMaxs);
-				searchRemIntervalLen = len % (nMaxs);
+		searchIntervalLen = len / (nMaxs);
+		searchRemIntervalLen = len % (nMaxs);
 		
-				int firstPassIterations = ( searchRemIntervalLen ) ?  nMaxs-1: nMaxs;
+		int firstPassIterations = ( searchRemIntervalLen ) ?  nMaxs-1: nMaxs;
 
-				startSearch = offset;
-				endSearch = startSearch + searchIntervalLen;				
+		startSearch = offset;
+		endSearch = startSearch + searchIntervalLen;				
 
-				for  ( int i = 0; i < firstPassIterations; i++ )
-				{
-						mMaxs[i] = *std::max_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+endSearch );
-						mMins[i] = *std::min_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+endSearch );
+		for  ( int i = 0; i < firstPassIterations; i++ )
+		{
+			mMaxs[i] = *std::max_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+endSearch );
+			mMins[i] = *std::min_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+endSearch );
 						
-						startSearch = endSearch;
-						endSearch += searchIntervalLen;	
-				}
-				
-				if ( searchRemIntervalLen )
-				{
-					mMaxs[nMaxs-1] = *std::max_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+startSearch+searchRemIntervalLen );
-					mMins[nMaxs-1] = *std::min_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+startSearch+searchRemIntervalLen );
-				}
+			startSearch = endSearch;
+			endSearch += searchIntervalLen;	
 		}
+				
+		if ( searchRemIntervalLen )
+		{
+			mMaxs[nMaxs-1] = *std::max_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+startSearch+searchRemIntervalLen );
+			mMins[nMaxs-1] = *std::min_element( mDataCached.GetPtr()+startSearch, mDataCached.GetPtr()+startSearch+searchRemIntervalLen );
+		}
+	}
 }
--- CLAM/src/Visualization/GL/SpectrumRenderingManager.cxx
+++ CLAM/src/Visualization/GL/SpectrumRenderingManager.cxx
@@ -4,95 +4,95 @@
 
 namespace CLAMVM
 {
-		SpectrumRenderingManager::SpectrumRenderingManager()
-				: mDrawAsLines( true ), mDrawAsDetail( false ), mDetailThreshold( 100 ), mMustProcessData( false ), mOffset(0), mLen(0)
-		{
+	SpectrumRenderingManager::SpectrumRenderingManager()
+		: mDrawAsLines( true ), mDrawAsDetail( false ), mDetailThreshold( 100 ), mMustProcessData( false ), mOffset(0), mLen(0)
+	{
 				
-		}
+	}
 
-		SpectrumRenderingManager::~SpectrumRenderingManager()
-		{
-		}
+	SpectrumRenderingManager::~SpectrumRenderingManager()
+	{
+	}
 		
-		void SpectrumRenderingManager::SetWorldBounds( const DataBoundBox& dataBBox )
-		{
-				mDataBBox = dataBBox;
-				mMustProcessData = true;
-		}
+	void SpectrumRenderingManager::SetWorldBounds( const DataBoundBox& dataBBox )
+	{
+		mDataBBox = dataBBox;
+		mMustProcessData = true;
+	}
 
-		void SpectrumRenderingManager::SetPixelBounds( const ScreenBoundBox& scrBBox )
-		{
-				mScreenBBox = scrBBox;
+	void SpectrumRenderingManager::SetPixelBounds( const ScreenBoundBox& scrBBox )
+	{
+		mScreenBBox = scrBBox;
 
-				mMustProcessData = true;
-		}
+		mMustProcessData = true;
+	}
 
-		void SpectrumRenderingManager::CacheData( const DataArray& data )
-		{
-				if ( mDataCached.Size() < data.Size() )
-						mDataCached.Resize( data.Size() );
-				mDataCached.SetSize( data.Size() );
+	void SpectrumRenderingManager::CacheData( const DataArray& data )
+	{
+		if ( mDataCached.Size() < data.Size() )
+			mDataCached.Resize( data.Size() );
+		mDataCached.SetSize( data.Size() );
 				
-				std::copy( data.GetPtr(), data.GetPtr()+data.Size(), mDataCached.GetPtr() );
+		std::copy( data.GetPtr(), data.GetPtr()+data.Size(), mDataCached.GetPtr() );
 				
-				mMustProcessData = true;				
-		}
+		mMustProcessData = true;				
+	}
 
-		void SpectrumRenderingManager::ActivateStraightRenderer()
-		{
-				mDrawAsLines = true;
-				mDrawAsDetail = false;
-		}
+	void SpectrumRenderingManager::ActivateStraightRenderer()
+	{
+		mDrawAsLines = true;
+		mDrawAsDetail = false;
+	}
 		
-		void SpectrumRenderingManager::ActivateDetailRenderer()
-		{
-				mDrawAsDetail = true;
-				mDrawAsLines = false;
-		}
+	void SpectrumRenderingManager::ActivateDetailRenderer()
+	{
+		mDrawAsDetail = true;
+		mDrawAsLines = false;
+	}
 
-		void SpectrumRenderingManager::ProcessData()
-		{
-				DetermineVisibleSamples();
+	void SpectrumRenderingManager::ProcessData()
+	{
+		DetermineVisibleSamples();
 				
-				if ( mProcessedData.Size() <= mDetailThreshold )
-				{
-						ActivateDetailRenderer();
-						mDetailRenderer.SetStartAndHopSize( mOffset, 1 );
-						mDetailRenderer.SetDataPtr( mProcessedData.GetPtr(), mProcessedData.Size() );
-				}
-				else
-				{
-						ActivateStraightRenderer();
-						mLineRenderer.SetStartAndHopSize( mOffset, 1 );
-						mLineRenderer.SetDataPtr( mProcessedData.GetPtr(), mProcessedData.Size() );
+		if ( mProcessedData.Size() <= mDetailThreshold )
+		{
+			ActivateDetailRenderer();
+			mDetailRenderer.SetStartAndHopSize( mOffset, 1 );
+			mDetailRenderer.SetDataPtr( mProcessedData.GetPtr(), mProcessedData.Size() );
+		}
+		else
+		{
+			ActivateStraightRenderer();
+			mLineRenderer.SetStartAndHopSize( mOffset, 1 );
+			mLineRenderer.SetDataPtr( mProcessedData.GetPtr(), mProcessedData.Size() );
 						
-				}
+		}
 				
-				mMustProcessData = false;
-		}
+		mMustProcessData = false;
+	}
 
-		void SpectrumRenderingManager::RenderData()
-		{
-				if ( mMustProcessData )
-						ProcessData();
-				if ( mDrawAsLines )
-						mLineRenderer.ExecuteGLCommands();
-				else
-						mDetailRenderer.ExecuteGLCommands();
-		}
+	void SpectrumRenderingManager::RenderData()
+	{
+		if ( mMustProcessData )
+			ProcessData();
+		if ( mDrawAsLines )
+			mLineRenderer.ExecuteGLCommands();
+		else
+			mDetailRenderer.ExecuteGLCommands();
+	}
 
-		void SpectrumRenderingManager::DetermineVisibleSamples()
-		{
+	void SpectrumRenderingManager::DetermineVisibleSamples()
+	{
 
-				mOffset = mDataBBox.mLeft;
-				mLen = (mDataBBox.mRight - mDataBBox.mLeft)+1;
+		mOffset = mDataBBox.mLeft;
+		mLen = (mDataBBox.mRight - mDataBBox.mLeft)+1;
 
-				if ( mProcessedData.Size() <= mLen )
-						mProcessedData.Resize( mLen+1 );
+		if ( mProcessedData.Size() <= mLen )
+			mProcessedData.Resize( mLen+1 );
 
-				mProcessedData.SetSize( mLen+1 );
+		mProcessedData.SetSize( mLen+1 );
 
-				std::copy( mDataCached.GetPtr()+mOffset, mDataCached.GetPtr()+mOffset+mLen+1, mProcessedData.GetPtr() );
+		std::copy( mDataCached.GetPtr()+mOffset, mDataCached.GetPtr()+mOffset+mLen+1, mProcessedData.GetPtr() );
 				
-		}
+	}
 }
