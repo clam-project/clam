9240763626195c326642ee68c90130a3a469e672
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-04-28 14:03:04 +0000

    + Bug fixed: ProcessingController::Publish() was not called when it was useful to call it
    git-svn-id: https://clam-project.org/clam/branches/development-branch@1975 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/View/TokenDelayController.cxx
+++ CLAM/src/Visualization/View/TokenDelayController.cxx
@@ -6,38 +6,36 @@ namespace CLAMVM
 {
 	using CLAM::TData;
 
-		SpectralTokenDelayController::SpectralTokenDelayController()
-		{
-		}
+	SpectralTokenDelayController::SpectralTokenDelayController()
+	{
+	}
 
-		SpectralTokenDelayController::~SpectralTokenDelayController()
-		{
-		}
+	SpectralTokenDelayController::~SpectralTokenDelayController()
+	{
+	}
 
-		bool SpectralTokenDelayController::BindTo( Processing& proc )
-		{
-				ProcessingController::Publish();
+	bool SpectralTokenDelayController::BindTo( Processing& proc )
+	{
+		ProcessingController::BindTo( proc );
 
-				mObserved = dynamic_cast<TokenDelay<Spectrum>* >( &proc );
+		mObserved = dynamic_cast<TokenDelay<Spectrum>* >( &proc );
 
-				CLAM_ASSERT( mObserved!=NULL, "Error: Trying to bind to a Processing that is not a TokenDelay<Spectrum>" );
+		CLAM_ASSERT( mObserved!=NULL, "Error: Trying to bind to a Processing that is not a TokenDelay<Spectrum>" );
 
-				return true;
-		}
+		return true;
+	}
 
-		bool SpectralTokenDelayController::Publish()
-		{
-				ProcessingController::Publish();
+	bool SpectralTokenDelayController::Publish()
+	{
 
-				ControlAdapter* adap = static_cast<ControlAdapter*> (mChildren["Delay Control"]);
+		ControlAdapter* adap = static_cast<ControlAdapter*> (mChildren["Delay Control"]);
 				
-				const CLAM::TokenDelayConfig& cfg = 
-					static_cast< const CLAM::TokenDelayConfig& >( mObserved->GetConfig() );
+		TData max = mObserved->MaxDelay();
 
-				TData max = cfg.GetMaxDelay();
-
-				adap->SetValueRange( 0, max );
+		adap->SetValueRange( 0, max );
 				
-				return true;
-		}
+		ProcessingController::Publish();
+
+		return true;
+	}
 }
