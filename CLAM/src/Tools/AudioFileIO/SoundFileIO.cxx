4ffd4aaf39454a6ba8beb0b08333936d060c24dd
mdeboer <mdeboer@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-05-05 14:53:20 +0000

    *** empty log message ***
    git-svn-id: https://clam-project.org/clam/branches/development-branch@2043 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Tools/AudioFileIO/SoundFileIO.cxx
+++ CLAM/src/Tools/AudioFileIO/SoundFileIO.cxx
@@ -238,6 +238,12 @@ void SoundFileIO::Seek(int pos)
 {
 	mPos = pos;
 
+	if (mPos==0)
+	{
+		rewind(mFile);
+		return;
+	}
+
 	if (mPos<0 || mPos>mSize)
 		throw ErrSoundFileIO("Position out of range");
 	
