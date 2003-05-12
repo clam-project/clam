a424df23ada4fbcd8d7e45c8e671540a2373566a
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-05-12 19:25:39 +0000

    Moved here
    git-svn-id: https://clam-project.org/clam/branches/development-branch@2137 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/test/testingWithCppUnit/NonComponentData/PolarTest.cxx
@@ -0,0 +1,140 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include "cppUnitHelper.hxx" // necessary for the custom assert
+
+#include "Polar.cxx"
+
+namespace CLAMTest
+{
+
+
+class PolarTest;
+
+CPPUNIT_TEST_SUITE_REGISTRATION( PolarTest );
+
+class PolarTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( PolarTest );
+	CPPUNIT_TEST( testConstructor_TakesValues );
+	CPPUNIT_TEST( testDefaultConstructor_InitAsZero );
+	CPPUNIT_TEST( testSetImag );
+	CPPUNIT_TEST( testSetReal );
+	CPPUNIT_TEST( testInequality_WithDifferentValues );
+	CPPUNIT_TEST( testStreamInsertion );
+	CPPUNIT_TEST( testStreamExtraction );
+	CPPUNIT_TEST( testStreamExtraction_WithSpacesToBeIgnored );
+	CPPUNIT_TEST( testStreamExtraction_WithADifferentStartToken );
+	CPPUNIT_TEST( testStreamExtraction_WithADifferentEndToken );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	/// Common initialization, executed before each test method
+	void setUp() { }
+
+	/// Common clean up, executed after each test method
+	void tearDown() { }
+
+private:
+
+	void testConstructor_TakesValues()
+	{
+		CLAM::Polar aPolar(3.0, 2.0);
+		CPPUNIT_ASSERT_EQUAL(3.0, aPolar.Real());
+		CPPUNIT_ASSERT_EQUAL(2.0, aPolar.Imag());
+	}
+
+	void testDefaultConstructor_InitAsZero()
+	{
+		CLAM::Polar zeroPolar(0.0, 0.0);
+		CLAM::Polar aDefaultConstructedPolar;
+
+		CPPUNIT_ASSERT_EQUAL(aDefaultConstructedPolar, zeroPolar);
+	}
+
+	void testInequality_WithDifferentValues()
+	{
+		CLAM::Polar pureRealUnitPolar(1.0, 0.0);
+		CLAM::Polar pureImagUnitPolar(0.0, 1.0);
+
+		CPPUNIT_ASSERT(pureImagUnitPolar != pureRealUnitPolar);
+	}
+
+	void testSetImag()
+	{
+		CLAM::Polar aPolar(3.0, 2.0);
+		aPolar.SetImag(4.3);
+		CPPUNIT_ASSERT_EQUAL(CLAM::Polar(3.0,4.3), aPolar);
+	}
+
+	void testSetReal()
+	{
+		CLAM::Polar aPolar(3.0, 2.0);
+		aPolar.SetReal(4.3);
+		CPPUNIT_ASSERT_EQUAL(CLAM::Polar(4.3,2.0), aPolar);
+	}
+
+
+	void testStreamInsertion()
+	{
+		std::stringstream s;
+		CLAM::Polar complex(1.453,3.454);
+		std::string expectedString("{1.453 3.454}");
+
+		s << complex << flush;
+
+		CPPUNIT_ASSERT_EQUAL(expectedString, s.str());
+	}
+
+	void testStreamExtraction()
+	{
+		CLAM::Polar toBeModified(1,3);
+		std::string inputString("{5.3 7.3}");
+		CLAM::Polar expected(5.3, 7.3);
+		std::stringstream ss(inputString);
+
+		ss >> toBeModified;
+
+		CPPUNIT_ASSERT_EQUAL(expected, toBeModified);
+	}
+
+	void testStreamExtraction_WithSpacesToBeIgnored()
+	{
+		CLAM::Polar toBeModified(1,3);
+		std::string inputString(" \n  { \n 5.3 \n  7.3 \t } ");
+		CLAM::Polar expected(5.3, 7.3);
+		std::stringstream ss(inputString);
+
+		ss >> toBeModified;
+
+		CPPUNIT_ASSERT_EQUAL(expected, toBeModified);
+	}
+
+	void testStreamExtraction_WithADifferentStartToken()
+	{
+		CLAM::Polar toBeModified(1.3,3.2);
+		CLAM::Polar expected    (1.3,3.2);
+		std::string inputString("a{5.3 7.3}");
+		std::stringstream ss(inputString);
+
+		ss >> toBeModified;
+
+		CPPUNIT_ASSERT_EQUAL(expected, toBeModified);
+	}
+
+	void testStreamExtraction_WithADifferentEndToken()
+	{
+		CLAM::Polar toBeModified(1.3,3.2);
+		CLAM::Polar expected    (1.3,3.2);
+		std::string inputString("{5.3 7.3a a");
+		std::stringstream ss(inputString);
+
+		ss >> toBeModified;
+
+		CPPUNIT_ASSERT_EQUAL(expected, toBeModified);
+	}
+
+};
+
+
+
+
+} // namespace CLAMTes
