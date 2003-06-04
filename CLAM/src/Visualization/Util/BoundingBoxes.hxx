5f5fe969be2b4e74da18f24959aebfaec1cccb99
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-06-04 16:56:31 +0000

    + Simple in/out test provided for the ScreenBoundBox
    git-svn-id: https://clam-project.org/clam/branches/development-branch@2398 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Visualization/Util/BoundingBoxes.hxx
+++ CLAM/src/Visualization/Util/BoundingBoxes.hxx
@@ -6,18 +6,26 @@ namespace CLAMVM
 
 struct DataBoundBox
 {
-		float mLeft;
-		float mRight;
-		float mBottom;
-		float mTop;
-		float mNear;
-		float mFar;				
+	float mLeft;
+	float mRight;
+	float mBottom;
+	float mTop;
+	float mNear;
+	float mFar;				
 };
 
 struct ScreenBoundBox
 {
-		int pixel_width;
-		int pixel_height;
+	int pixel_left;
+	int pixel_top;
+	int pixel_width;
+	int pixel_height;
+
+	inline bool IsPointInside( int ex, int ey ) {
+
+		return ( ex > pixel_left && ex < (pixel_left + pixel_width) ) 
+			&& ( ey > pixel_top && ey < ( pixel_top + pixel_height ) );
+	}
 };
 
 }
