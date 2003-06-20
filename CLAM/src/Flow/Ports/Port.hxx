29c485f561336109db905eed3b28fd887e044a81
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-06-20 15:53:40 +0000

    +  removed assert in SetParams
    git-svn-id: https://clam-project.org/clam/branches/development-branch@2548 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Flow/Ports/Port.hxx
+++ CLAM/src/Flow/Ports/Port.hxx
@@ -86,7 +86,12 @@ namespace CLAM {
 		void SetParams(unsigned int length,
 		               unsigned int hop = 0)
 		{
-			CLAM_ASSERT( !IsAttached(), "Port::SetParams(int), SetParams must be called before Attach port" );
+			//CLAM_ASSERT( !IsAttached(), "Port::SetParams(int), SetParams must be called before Attach port" );
+
+			// PA: todo: allow attach before a SetParams. For this we need to finish the Nodes refactoring
+			// by now I comment this assert because it doesn't allows SpectralDelay to work.
+			// a cludge (which is not rapid) would be to add a new virtual IsAttachedToNode() which would
+			// be a better assert.
 			mLength = length;
 			mHop = hop?hop:length;
 		}
