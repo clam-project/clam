f9abb379a6c4bf2dcb0ed1ef835a52a7acb6a9e0
(no author) <(no author)@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-06-30 09:24:40 +0000

    This commit was manufactured by cvs2svn to create tag 'CLAM-post-Albert-Merge-from-Draft-rel'.
    git-svn-id: https://clam-project.org/clam/tags/CLAM-post-Albert-Merge-from-Draft-rel@2621 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/build/srcdeps/objdepname.h
+++
@@ -1,9 +0,0 @@
-#ifndef __objdepname__
-#define __objdepname__
-
-char* extension(char* filename);
-
-void convert_to_objname(char* objname,int maxlen,const char* filename);
-void convert_to_depname(char* depname,int maxlen,const char* filename);
-
-#endif
