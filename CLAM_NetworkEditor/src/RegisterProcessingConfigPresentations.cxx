7720bda07d250d662c4f40f2471b50654bbb5b59
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-10-02 10:33:53 +0000

    Added processing config presentations to the factory
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3223 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM_NetworkEditor/src/RegisterProcessingConfigPresentations.cxx
+++ CLAM_NetworkEditor/src/RegisterProcessingConfigPresentations.cxx
@@ -1,15 +1,34 @@
 
 #include "Factory.hxx"
 #include "ProcessingConfigPresentation.hxx"
-#include "Qt_ProcessingConfigPresentation.hxx"
+
+// concrete configs
+#include "OscillatorConfigPresentation.hxx"
+#include "ConfigPresentationTmpl.hxx"
+#include "AudioOutWrapper.hxx"
+#include "Controller.hxx"
+#include "AudioFile.hxx"
+#include "AudioIO.hxx"
+#include "AudioMixer.hxx"
+#include "BinaryAudioOp.hxx"
+#include "Oscillator.hxx"
 
 typedef CLAM::Factory<NetworkGUI::ProcessingConfigPresentation> ProcessingConfigPresentationFactory;
 
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regAudioOutWrapperCfg( "AudioOutWrapperConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regAutoPannerCfg( "AutoPannerConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regControllerCfg( "ControllerConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regAudioFileInCfg( "AudioFileConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regAudioOutCfg( "AudioIOConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regOscillatorCfg( "OscillatorConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regAudioMixerCfg( "AudioMixerConfig" );
-static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::Qt_ProcessingConfigPresentation> regMultiplierCfg( "BinaryAudioOpConfig" );
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::AudioOutWrapperConfig> AudioOutWrapperConfigPresentation;
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::ControllerConfig> ControllerConfigPresentation;
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::AudioFileConfig> AudioFileConfigPresentation;
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::AudioIOConfig> AudioIOConfigPresentation;
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::AudioMixerConfig> AudioMixerConfigPresentation;
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::BinaryAudioOpConfig> BinaryAudioOpConfigPresentation;
+typedef NetworkGUI::ConfigPresentationTmpl<CLAM::OscillatorConfig> OscillatorConfigPresentationTmpl;
+
+static ProcessingConfigPresentationFactory::Registrator<AudioOutWrapperConfigPresentation> regAudioOutWrapperCfg( "AudioOutWrapperConfig" );
+static ProcessingConfigPresentationFactory::Registrator<ControllerConfigPresentation> regAutoPannerCfg( "AutoPannerConfig" );
+static ProcessingConfigPresentationFactory::Registrator<AudioFileConfigPresentation> regControllerCfg( "ControllerConfig" );
+static ProcessingConfigPresentationFactory::Registrator<AudioIOConfigPresentation> regAudioFileInCfg( "AudioFileConfig" );
+static ProcessingConfigPresentationFactory::Registrator<AudioIOConfigPresentation> regAudioOutCfg( "AudioIOConfig" );
+static ProcessingConfigPresentationFactory::Registrator<NetworkGUI::OscillatorConfigPresentation> regOscillatorCfg( "OscillatorConfig" );
+//static ProcessingConfigPresentationFactory::Registrator<OscillatorConfigPresentationTmpl> regOscillatorCfg( "OscillatorConfig" );
+static ProcessingConfigPresentationFactory::Registrator<AudioMixerConfigPresentation> regAudioMixerCfg( "AudioMixerConfig" );
+static ProcessingConfigPresentationFactory::Registrator<BinaryAudioOpConfigPresentation> regMultiplierCfg( "BinaryAudioOpConfig" );
