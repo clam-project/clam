be17694520d877958dbe8e91eac149c407c4b33f
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-10-21 19:18:02 +0000

    Error in documentation
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3312 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Standard/Text.hxx
+++ CLAM/src/Standard/Text.hxx
@@ -10,7 +10,7 @@ namespace CLAM
 	/**
 	 * This class represents an string that can contain any kind of separators.
 	 * Use it instead std::string if you want to deserialize
-	 * Inherit all the std::string methods.
+	 * It inherits all the std::string methods.
 	 */
 	class Text : public std::string
 	{
