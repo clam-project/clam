5d1c0d928f16b714e6d94a80a2eeb10d7098159c
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-10-22 13:57:24 +0000

    + Visual do not allow using typedef 'string' as base class + Visual do not uses << operator over a istream when applied to a istringstream + Visual do not implement string::push_back + Optimized method for iterator less extraction
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3313 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Standard/Text.hxx
+++ CLAM/src/Standard/Text.hxx
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>
+#include <iterator>
 #include "TypeInfo.hxx"
 
 namespace CLAM
@@ -12,18 +13,25 @@ namespace CLAM
 	 * Use it instead std::string if you want to deserialize
 	 * It inherits all the std::string methods.
 	 */
-	class Text : public std::string
+	// MRJ: I was obliged to change the alias usage std::string by the long and quite unreadable
+	// basic_string<char> thing ( thanks Pau! ) in order to get Text to compile under VisualC++ 6.0. The compiler
+	// was issuing this error ( pure nonsense BTW ):
+	// src\Standard\Text.hxx(29) : error C2614: 'Text' : illegal member initialization: 'string' is not a base or member
+	// src\Standard\Text.hxx(33) : error C2614: 'Text' : illegal member initialization: 'string' is not a base or member
+	// I don't like this much, but AFAIK there's no way of 'undoing' a using statement.
+
+	class Text : public std::basic_string<char>
 	{
 		public:
 			Text()
 			{
 			}
 			Text(const std::string & value)
-				: std::string(value)
+				: std::basic_string<char>(value)
 			{
 			}
 			Text(const char * value)
-				: std::string(value)
+				: std::basic_string<char>(value)
 			{
 			}
 	};
@@ -33,25 +41,45 @@ namespace CLAM
 		
 #if 0
 		stream.unsetf(std::ios::skipws);
-		text.assign(
+		text = std::string(
 			(std::istream_iterator<char>(stream)),
 			std::istream_iterator<char>());
 #elif 0
 		text.assign(
 			(std::istreambuf_iterator<char>(stream)),
 			std::istreambuf_iterator<char>());
-#elif 1
+#elif 0
 		text="";
 		char c;
 		while (stream.get(c))
 			text.push_back(c);
+//			text.append(1,c);
+#elif 1
+		text = "";
+		char buffer[1024];
+		stream.read( buffer, 1023 );
+		do 
+		{
+			text.append( buffer, stream.gcount() );
+		}
+		while( stream.read( buffer, 1023 ) );
 #endif
 		return stream;
 
 	}
 
-	CLAM_TYPEINFOGROUP(BasicCTypeInfo, Text);
+#if 1
+	/**
+	* This is a kludge to make windows work
+	*/
+	inline std::istream& operator>>( std::istringstream& stream, CLAM::Text& text )
+	{
+		std::istream & basestream = stream;
+		return basestream >> text;
+	}
+#endif
 
+	CLAM_TYPEINFOGROUP(BasicCTypeInfo, Text);
 }
 
 
