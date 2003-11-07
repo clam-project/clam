3a5c52c94df5c4f73c269fd905f93ced58d3a89c
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-11-07 19:29:28 +0000

    Squeleton
    git-svn-id: https://clam-project.org/clam/branches/xerces2-porting-branch@3452 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/src/Storage/XML/ClamObjects2XercesDom.hxx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2001-2002 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _CLAM_OBJECTS_2_XERCES_DOM_HXX_
+#define _CLAM_OBJECTS_2_XERCES_DOM_HXX_
+
+#include "XercesEncodings.hxx"
+#include "XMLable.hxx"
+#include <xercesc/dom/DOMNode.hpp>
+
+
+#include <vector>
+
+namespace xercesc = XERCES_CPP_NAMESPACE;
+
+namespace CLAM
+{
+
+class ClamObject2XercesDom
+{
+	DOMNode * node;
+public:
+	void Store(XMLable & xmlable)
+	{
+	}
+
+	xercesc::DOMNode * getDom()
+	{
+		return 0;
+	}
+
+	
+};
+
+	
+}
+
+
+#endif//_CLAM_OBJECTS_2_XERCES_DOM_HXX_
