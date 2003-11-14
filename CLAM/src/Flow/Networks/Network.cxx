7f38974238dccf8f9650dcd4bba27e7c95b046d4
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-11-14 10:38:26 +0000

    + Added GPL headers
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3480 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Flow/Networks/Network.cxx
+++ CLAM/src/Flow/Networks/Network.cxx
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2001-2003 MUSIC TECHNOLOGY GROUP (MTG)
+ *                         UNIVERSITAT POMPEU FABRA
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 
 #include "Network.hxx"
 #include "FlowControl.hxx"
@@ -437,8 +458,6 @@ namespace CLAM
 
 	void Network::Clear()
 	{
-//		Stop();
-
 		Nodes::iterator itNodes;
 		for(itNodes=BeginNodes();itNodes!=EndNodes();itNodes++)
 			delete *itNodes;
