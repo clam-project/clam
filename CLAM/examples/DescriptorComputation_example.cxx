0a08e290825dc6d549268b412ee3356c14d558e0
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-11-24 12:40:38 +0000

    Updated all Descriptor general infrastructure
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3510 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/examples/DescriptorComputation_example.cxx
@@ -0,0 +1,68 @@
+
+#include "Segment.hxx"
+#include "XMLStorage.hxx"
+#include "SegmentDescriptors.hxx"
+#include "DescriptorComputation.hxx"
+
+using namespace CLAM;
+
+
+int main()
+{
+		XMLStorage s;
+		Segment segment;
+		std::cout<<"Please enter name of your xml analysis file"<<"\n";
+		std::string filename;
+		std::cin>>filename;
+		std::cout<<"Loading Analysis File. Please Wait."<<"\n";
+		s.Restore(segment,filename);
+		std::cout<<"Analysis File Loaded Successfully"<<"\n";
+
+		std::cout<<"Computing Descriptors. Please Wait."<<"\n";
+		
+		
+		
+		SegmentDescriptors segmentDescriptors;
+
+		SpectralDescriptors specProto;
+		specProto.AddMean();
+		specProto.AddCentroid();
+		specProto.AddEnergy();
+		specProto.UpdateData();
+
+		SpectralPeakDescriptors peakProto;
+		peakProto.AddMagnitudeMean();
+		peakProto.AddHarmonicCentroid();
+		peakProto.UpdateData();
+
+		FrameDescriptors frameProto;
+		frameProto.AddSpectralPeakD();
+		frameProto.AddResidualSpecD();
+		frameProto.UpdateData();
+		
+		frameProto.GetSpectralPeakD().SetPrototype(peakProto);
+		frameProto.GetResidualSpecD().SetPrototype(specProto);
+		
+		segmentDescriptors.SetFramePrototype(frameProto,segment.GetnFrames());
+		segmentDescriptors.SetpSegment(&segment);
+		
+
+		//segmentDescriptors.Compute();
+		
+		DescriptorComputation processing;
+		processing.Do(segmentDescriptors);
+		
+		std::cout<<"Descriptors Computed Successfully"<<"\n";
+		
+		std::cout<<"Please enter name of where you want your output descriptors to be stored"<<"\n";
+		std::string outFilename;
+		std::cin>>outFilename;
+
+		std::cout<<"Storing Results into xml file. Please Wait."<<"\n";
+		s.Dump(segmentDescriptors,"segmentDescriptors",outFilename);
+		
+		std::cout<<"Program finished Successfully"<<"\n";
+		return 0;
+}
+
+  
