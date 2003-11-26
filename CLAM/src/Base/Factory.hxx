c3253eff2a81381e97484fb8359dda23f8bc26cc
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-11-26 12:33:13 +0000

    Fixed previous blunder
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3568 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Base/Factory.hxx
+++ CLAM/src/Base/Factory.hxx
@@ -185,20 +185,6 @@ public: // Inner classes. Public for better testing
 			return new ConcreteProductType;
 		}
 
-
-		// MRJ: The following two overloads exist only with testing purposes
-		Registrator( RegistryKey key, TheFactoryType*& pointerToUsedSingleton ) {
-			TheFactoryType::GetInstance().AddCreator( key, Create );
-			pointerToUsedSingleton = &TheFactoryType::GetInstance();
-		}
-
-		Registrator( TheFactoryType*& pointerToUsedSingleton ) {
-			ConcreteProductType dummy;
-			TheFactoryType::GetInstance().AddCreator( dummy.GetClassName(), Create );
-			pointerToUsedSingleton = &TheFactoryType::GetInstance();
-		}
-
-
 	};
 
 private:
