a6d5460750857ccd53f8554f7b9304f5850297d6
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2003-12-10 17:03:47 +0000

    + added draft new processings to this file, in order to not add them in src/Processing/DefaultProcessingFactoryRegistration.cxx
    git-svn-id: https://clam-project.org/clam/branches/development-branch@3606 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM_NetworkEditor/src/MoreProcessingFactoryRegistration.cxx
@@ -0,0 +1,13 @@
+
+#include "Factory.hxx"
+// processings in draft directory
+#include "Random.hxx"
+#include "FlagControl.hxx"
+#include "OneOverF.hxx"
+
+typedef CLAM::Factory<CLAM::Processing> ProcessingFactory;
+
+// processings in draft directory
+static ProcessingFactory::Registrator<CLAM::FlagControl> regtFlagControl( "FlagControl" );
+static ProcessingFactory::Registrator<CLAM::Random> regtRandom( "Random" );
+static ProcessingFactory::Registrator<CLAM::OneOverF> regtOneOverF( "OneOverF" );
