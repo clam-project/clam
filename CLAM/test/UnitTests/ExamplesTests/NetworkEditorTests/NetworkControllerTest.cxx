51f908918bf37e2f5bd0a277e099f26a33772f5d
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-02-24 11:52:59 +0000

    + new way to test network editor
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4341 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/UnitTests/ExamplesTests/NetworkEditorTests/NetworkControllerTest.cxx
+++ CLAM/test/UnitTests/ExamplesTests/NetworkEditorTests/NetworkControllerTest.cxx
@@ -21,16 +21,11 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include "BaseLoggable.hxx" // also includes <sstream>
-#include "Network.hxx"
-#include "NetworkController.hxx"
-#include "SimpleOscillator.hxx"
-#include "AutoPanner.hxx"
-#include "AudioMultiplier.hxx"
-#include "PushFlowControl.hxx"
-#include "Processing.hxx"
-#include "Signalv1.hxx"
+#include "NetworkPresentation.hxx"
+#include "ProcessingController.hxx"
+#include "ConnectionAdapter.hxx"
+#include <string>
 
-#include <iostream>
 namespace CLAMTest 
 {
 
@@ -38,171 +33,44 @@ class NetworkControllerTest;
 CPPUNIT_TEST_SUITE_REGISTRATION( NetworkControllerTest );
 
 
-class NetworkControllerTest : public CppUnit::TestFixture, public CLAMVM::NetworkController
+class NetworkControllerTest : public CppUnit::TestFixture, public NetworkGUI::NetworkPresentation
 {
+public:
+	virtual void Show()
+	{
+	}
+	
+	virtual void Hide()
+	{
+	}
+	
+protected:
+	void SetName(const std::string& name)
+	{
+	}
+	
+	void CreateProcessingPresentation( const std::string &, CLAMVM::ProcessingController * )
+	{
+	}
+	
+	void CreatePortConnectionPresentation( CLAMVM::ConnectionAdapter * )
+	{
+	}
+	
+	void CreateControlConnectionPresentation( CLAMVM::ConnectionAdapter * )
+	{
+	}
+	
 	CPPUNIT_TEST_SUITE( NetworkControllerTest );
 		
-	CPPUNIT_TEST( testNetworkControllerStopsNetwork_WhenIsStopped );
-	CPPUNIT_TEST( testAddProcessingCreatesProcessingController );
-	CPPUNIT_TEST( testAddProcessingThrowsException_WhenDuplicatedName );
-	CPPUNIT_TEST( testRemoveProcessingDeletesProcessingController );
-	CPPUNIT_TEST( testRemoveProcessingThrowsException_WhenProcessingControllerDoesntExist );
-	CPPUNIT_TEST( testProcessingNameChangedModifiesProcessingControllerMap );
-	CPPUNIT_TEST( testProcessingNameChangedModifiesPortConnectionControllerMap );
-	CPPUNIT_TEST( testProcessingNameChangedModifiesControlConnectionControllerMap );
-	CPPUNIT_TEST( testChangeKeyMapDoesntModify_WhenRepeatedKey );
-
+	CPPUNIT_TEST( test );
 	
 	CPPUNIT_TEST_SUITE_END();
 
-	CLAM::Network * mNetwork;
-	
 public:
-	void setUp() 
-	{	
-		mNetwork = new CLAM::Network;
-		const int nodeSize=1;
-		mNetwork->AddFlowControl( new CLAM::PushFlowControl(nodeSize) );
-		
- 		BindTo(*mNetwork);	
-	}
-	
-	void tearDown() 
+	void test()
 	{
-		mLoopCondition = false;
-		
-		mThread.Stop();
-		if (mObserved)
-			mObserved->Stop();
-
-		Clear();
-		mObserved = 0;
-		delete mNetwork;
-
 	}
-	
-	void testNetworkControllerStopsNetwork_WhenIsStopped()
-	{
-		SigSlot::Signalv1<bool> SignalChangeState;
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		mNetwork->AddProcessing( "oscillator", oscillator) ;
-
-		SignalChangeState.Connect( SlotChangeState );
-		
-		SignalChangeState.Emit(true);
-		CPPUNIT_ASSERT_EQUAL( CLAM::Processing::Running, oscillator->GetExecState() );
-		
-		SignalChangeState.Emit(false);
-		CPPUNIT_ASSERT_EQUAL( CLAM::Processing::Ready, oscillator->GetExecState() );
-		SlotChangeState.Unbind();
-	}
-
-	void testAddProcessingCreatesProcessingController()
-	{
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		AddProcessing( "oscillator", oscillator );
-				
-		CPPUNIT_ASSERT_EQUAL( 1, (int)mProcessingControllers.size() );
-	}
-
-	void testAddProcessingThrowsException_WhenDuplicatedName()
-	{
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		CLAM::SimpleOscillator * oscillator2 = new CLAM::SimpleOscillator;
-
-		AddProcessing( "oscillator", oscillator );
-		try
-		{
-			AddProcessing( "oscillator", oscillator2 );
-			CPPUNIT_FAIL("exception should be thrown when duplicating key");
-		}
-		catch(CLAM::ErrAssertionFailed &)
-		{			
-		}	
-	}
-
-	void testRemoveProcessingDeletesProcessingController()
-	{
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		AddProcessing( "oscillator", oscillator );
-		RemoveProcessing( "oscillator" ); // TODO:: its name should be RemoveProcessing
-
-		CPPUNIT_ASSERT_EQUAL( 0, (int)mProcessingControllers.size() );
-	}
-
-	void testRemoveProcessingThrowsException_WhenProcessingControllerDoesntExist()
-	{
-		try
-		{
-			RemoveProcessing("oscillator");
-			CPPUNIT_FAIL("exception should be thrown when trying to remove a nonexistent key");
-		}
-		catch(CLAM::ErrAssertionFailed &)
-		{
-		}
-	}
-
-	void testProcessingNameChangedModifiesProcessingControllerMap()
-	{	
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		AddProcessing( "oscillator", oscillator );
-		ProcessingControllersMapIterator it = mProcessingControllers.find("oscillator");
-		ProcessingNameChanged( "mynewname", it->second );
-		CPPUNIT_ASSERT_EQUAL( it->second, mProcessingControllers.find( "mynewname" )->second );
-	}
-
-	void testProcessingNameChangedModifiesPortConnectionControllerMap()
-	{
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		CLAM::AudioMultiplier * multiplier = new CLAM::AudioMultiplier;
-		
-		AddProcessing( "oscillator", oscillator );
-		AddProcessing( "multiplier", multiplier );
-		
-		CreatePortConnection( "oscillator.Audio Output", "multiplier.First Audio Input" );
-
-		ProcessingControllersMapIterator it = mProcessingControllers.find("oscillator");
-		ProcessingNameChanged( "mynewname", it->second );
-
-		ConnectionAdapterIterator itc;
-		itc = mConnectionAdapters.begin();
-		std::string newName( "mynewname.Audio Output" );
-		CPPUNIT_ASSERT_EQUAL( newName, (*itc)->GetOutName() );
-	}
-	void testProcessingNameChangedModifiesControlConnectionControllerMap()
-	{
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		CLAM::AutoPanner * panner = new CLAM::AutoPanner;
-		
-		AddProcessing( "oscillator", oscillator );
-		AddProcessing( "panner", panner );
-		
-		CreateControlConnection( "panner.Left Control", "oscillator.Amplitude" );
-
-		ProcessingControllersMapIterator it = mProcessingControllers.find("oscillator");
-		ProcessingNameChanged( "mynewname", it->second );
-
-		ConnectionAdapterIterator itc;
-		itc = mConnectionAdapters.begin();
-		std::string newName ("mynewname.Amplitude");
-		CPPUNIT_ASSERT_EQUAL( newName, (*itc)->GetInName() );
-	}
-
-
-
-	void testChangeKeyMapDoesntModify_WhenRepeatedKey()
-	{	
-		CLAM::SimpleOscillator * oscillator = new CLAM::SimpleOscillator;
-		CLAM::SimpleOscillator * oscillator2 = new CLAM::SimpleOscillator;
-		AddProcessing( "oscillator", oscillator );
-		AddProcessing( "repeated name", oscillator2 );
-		
-		CPPUNIT_ASSERT_EQUAL( false, ChangeKeyMap( "oscillator", "repeated name") );
-	}
-
-
-			
-
 };
 
 } // namespace CLAMTest
