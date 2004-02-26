d8fd72d0f83c1b6dfd2c07676b573ca8eb9a9f41
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-02-26 17:38:40 +0000

    Only pools
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4353 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Data/Descriptors/Pool/Pool.hxx
+++ CLAM/src/Data/Descriptors/Pool/Pool.hxx
@@ -1,102 +1,10 @@
-#include <vector>
 #include <string>
-#include "StaticBool.hxx"
-
+#include <map>
 #include "DataTypes.hxx"
+
 
 namespace CLAM
 {
-	template <typename baseType>
-	struct vectorialExpressionTrait
-	{
-		typedef StaticTrue isVector;
-		typedef baseType expressionType;
-	};
-	template <typename baseType>
-	struct scalarExpressionTrait
-	{
-		typedef StaticFalse isVector;
-		typedef baseType expressionType;
-	};
-
-	template <typename fT, typename gT>
-	struct composition 
-	{
-		typedef fT fType;
-		typedef gT gType;
-		typedef typename gT::paramType paramType;
-		typedef typename fT::resultType resultType;
-
-		// Those two methods should be inherited from unary
-		template <typename gType2>
-		CLAM::composition<composition<fType,gType>,gType2> operator()(const gType2 & g)
-		{
-			return compose((composition<fType,gType>&)(*this),g);
-		}
-		resultType operator()(const paramType & x)
-		{
-			return apply(x);
-		}
-
-		composition(const composition<fType,gType> & other)
-			: _f(other._f), _g(other._g) {}
-
-
-		composition(const fType & f, const gType & g)
-			: _f(f), _g(g) {}
-
-		resultType apply(const paramType & x)
-		{
-			return _f.apply(_g.apply(x));
-		}
-	private:
-		fType _f;
-		gType _g;
-	};
-
-	template <typename fType, typename gType>
-	composition<fType,gType> compose(const fType & f, const gType & g)
-	{
-		return composition<fType,gType>(f,g);
-	}
-
-	template <typename ConcreteType>
-	struct unary
-	{
-		typedef TData paramType;
-		typedef TData resultType;
-
-		template <typename gType>
-		CLAM::composition<ConcreteType,gType> operator()(const gType & g)
-		{
-			return compose((ConcreteType&)(*this),g);
-		}
-		resultType operator()(const paramType & x)
-		{
-			return ((ConcreteType*)this)->ConcreteType::apply(x);
-		}
-
-	};
-
-	struct abs : public unary<abs>
-	{
-		resultType apply(const paramType & x)
-		{
-			return std::abs(x);
-		}
-	};
-
-
-	struct neg : public unary<neg>
-	{
-		resultType apply(const paramType & x)
-		{
-			return -(x);
-		}
-	};
-
-
-
 
 	class CalculationSpec
 	{
@@ -126,13 +34,14 @@ namespace CLAM
 		
 	};
 
+/*
 	class Scope
 	{
 		ScopeSpec * _spec;
 	public:
 	};
 
-
+*/
 }
 
 
--- CLAM/src/Data/Descriptors/Pool/PoolTest.cxx
+++ CLAM/src/Data/Descriptors/Pool/PoolTest.cxx
@@ -8,10 +8,6 @@
 namespace CLAMTest
 {
 
-CLAM::TData testingDoubler(CLAM::TData)
-{
-}
-
 class PoolTest;
 
 CPPUNIT_TEST_SUITE_REGISTRATION( PoolTest );
@@ -21,14 +17,6 @@ class PoolTest : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE( PoolTest );
 	CPPUNIT_TEST( testCalculationInserted );
 	CPPUNIT_TEST( testCalculationCalculed );
-	CPPUNIT_TEST( testApplyUnaryFunctionAbs );
-	CPPUNIT_TEST( testApplyUnaryFunctionNeg );
-	CPPUNIT_TEST( testApplyUnaryComposition );
-	CPPUNIT_TEST( testApplyUnaryCompositionInverted );
-	CPPUNIT_TEST( testComposingWithApplyOperator );
-	CPPUNIT_TEST( testComposingWithApplyOperatorInverted );
-	CPPUNIT_TEST( testComposingCompositions );
-	CPPUNIT_TEST( testPows );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -54,92 +42,6 @@ private:
 		CPPUNIT_ASSERT_EQUAL(std::string("Lala\n"),spec.DescriptorsList());
 	}
 	
-	void testApplyUnaryFunctionAbs()
-	{
-		CLAM::abs myAbs;
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, myAbs(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, myAbs(2.0),.0001);
-	}
-
-	void testApplyUnaryFunctionNeg()
-	{
-		CLAM::neg myNeg;
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, myNeg(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-2, myNeg(2.0),.0001);
-	}
-
-	void testApplyUnaryComposition()
-	{
-		CLAM::neg myNeg;
-		CLAM::abs myAbs;
-
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-2, CLAM::compose (myNeg,myAbs)(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-2, CLAM::compose (myNeg,myAbs)(2.0),.0001);
-	}
-
-	void testApplyUnaryCompositionInverted()
-	{
-		CLAM::neg myNeg;
-		CLAM::abs myAbs;
-
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, CLAM::compose (myAbs,myNeg)(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, CLAM::compose (myAbs,myNeg)(2.0),.0001);
-	}
-
-	void testComposingWithApplyOperator()
-	{
-		CLAM::neg myNeg;
-		CLAM::abs myAbs;
-
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-2, myNeg(myAbs)(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-2, myNeg(myAbs)(2.0),.0001);
-	}
-
-	void testComposingWithApplyOperatorInverted()
-	{
-		CLAM::neg myNeg;
-		CLAM::abs myAbs;
-
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, myAbs(myNeg)(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(2, myAbs(myNeg)(2.0),.0001);
-	}
-
-	void testComposingCompositions()
-	{
-		CLAM::neg myNeg;
-		CLAM::abs myAbs;
-		CLAM::cubed myCubed;
-
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-8, myCubed(myNeg(myAbs))(-2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(-8, myCubed(myNeg(myAbs))(2.0),.0001);
-	}
-
-	void testPows()
-	{
-		CLAM::pow<0> pow0;
-		CLAM::pow<1> identity;
-		CLAM::pow<2> squared;
-		CLAM::pow<3> cubed;
-		CLAM::pow<4> pow4;
-		CLAM::pow<5> pow5;
-		CLAM::pow<6> pow6;
-		CLAM::pow<7> pow7;
-		CLAM::pow<8> pow8;
-		CLAM::pow<9> pow9;
-		CLAM::pow<37> pow37;
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(  1, pow0(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(  2, identity(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(  4, squared(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(  8, cubed(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL( 16, pow4(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL( 32, pow5(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL( 64, pow6(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(128, pow7(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(256, pow8(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(512, pow9(2.0),.0001);
-		CPPUNIT_ASSERT_DOUBLES_EQUAL(137438953472., pow37(2.0),.0001);
-	}
-
 	void testCalculationCalculed()
 	{
 		CLAM::TData data[10] = { 0,1,-2,3,-4,5,-6,7,-8,9 };
