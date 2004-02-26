18c63b94b3aeb10641120a0ee914884830547405
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-02-26 17:40:19 +0000

    Moved from pools Added binary functors
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4354 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/src/Data/Descriptors/Pool/Functors.hxx
@@ -0,0 +1,175 @@
+#include <cmath>
+//#include "StaticBool.hxx"
+
+//#include "DataTypes.hxx"
+
+
+namespace CLAM
+{
+	typedef double TData;
+/*
+	template <typename baseType>
+	struct vectorialExpressionTrait
+	{
+		typedef StaticTrue isVector;
+		typedef baseType expressionType;
+	};
+	template <typename baseType>
+	struct scalarExpressionTrait
+	{
+		typedef StaticFalse isVector;
+		typedef baseType expressionType;
+	};
+*/
+	template <typename ConcreteType> struct unary;
+
+	template <typename fT, typename gT>
+	struct composition : public unary< composition<fT,gT> >
+	{
+		typedef fT fType;
+		typedef gT gType;
+		typedef typename gT::paramType paramType;
+		typedef typename fT::resultType resultType;
+
+		composition()
+			: _f(fType()), _g(gType()) {}
+		composition(const composition<fType,gType> & other)
+			: _f(other._f), _g(other._g) {}
+		composition(const fType & f, const gType & g)
+			: _f(f), _g(g) {}
+
+		resultType apply(const paramType & x)
+		{
+			return _f.apply(_g.apply(x));
+		}
+	private:
+		fType _f;
+		gType _g;
+	};
+
+	template <typename fType, typename gType>
+	composition<fType,gType> compose(const fType & f, const gType & g)
+	{
+		return composition<fType,gType>(f,g);
+	}
+
+	template <typename ConcreteType>
+	struct unary
+	{
+		typedef TData paramType;
+		typedef TData resultType;
+
+		template <typename gType>
+		CLAM::composition<ConcreteType,gType> operator()(const gType & g)
+		{
+			return compose((ConcreteType&)(*this),g);
+		}
+		resultType operator()(const paramType & x)
+		{
+			return ((ConcreteType*)this)->ConcreteType::apply(x);
+		}
+	};
+
+
+	template <typename ConcreteType>
+	struct binary
+	{
+		typedef TData param1Type;
+		typedef TData param2Type;
+		typedef TData resultType;
+/*
+		template <typename gType>
+		CLAM::composition<ConcreteType,gType> operator()(const gType & g)
+		{
+			return compose((ConcreteType&)(*this),g);
+		}
+*/
+		resultType operator()(const param1Type & x1, const param2Type & x2)
+		{
+			return ((ConcreteType*)this)->ConcreteType::apply(x1,x2);
+		}
+	};
+
+	struct sum : public binary<sum>
+	{
+		resultType apply(const param1Type & x1, const param2Type & x2)
+		{
+			return x1+x2;
+		}
+	};
+
+	struct prod : public binary<prod>
+	{
+		resultType apply(const param1Type & x1, const param2Type & x2)
+		{
+			return x1*x2;
+		}
+	};
+
+	struct abs : public unary<abs>
+	{
+		resultType apply(const paramType & x)
+		{
+			return std::abs(x);
+		}
+	};
+
+
+	struct neg : public unary<neg>
+	{
+		resultType apply(const paramType & x)
+		{
+			return -(x);
+		}
+	};
+
+	template <unsigned n>
+	struct pow : public unary<pow<n> >
+	{
+		typedef TData resultType;
+		typedef TData paramType;
+		resultType apply(const paramType & x)
+		{
+			return power(x,(isOdd*)0);
+		}
+		static resultType squared(const paramType & x)
+		{
+			return x*x;
+		}
+
+		template <unsigned isOdd> class isOddSelector {};
+		typedef isOddSelector<(n&1)> isOdd;
+
+		static resultType power(const paramType & x, isOddSelector<1> * foo)
+		{
+			typedef pow<(n>>1)> nextPower;
+			typedef typename nextPower::isOdd nextIsOdd;
+			return squared(nextPower::power(x,(nextIsOdd*)0)) * x;
+		}
+		static resultType power(const paramType & x, isOddSelector<0> * foo)
+		{
+			typedef pow<(n>>1)> nextPower;
+			typedef typename nextPower::isOdd nextIsOdd;
+			return squared(nextPower::power(x,(nextIsOdd*)0));
+		}
+	};
+
+	template<>
+	pow<1>::resultType pow<1>::power(const paramType & x, pow<1>::isOddSelector<1> * foo)
+	{
+		return x;
+	}
+	template<>
+	pow<0>::resultType pow<0>::power(const paramType & x, pow<0>::isOddSelector<0> * foo)
+	{
+		return 1;
+	}
+
+	typedef pow<2> squared;
+	typedef pow<3> cubed;
+
+
+}
+
+
+
