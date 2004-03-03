ea04e675a2d4d92a06edae94ff069ae8a2d439b8
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-03 19:06:47 +0000

    Adding attributes to a PoolSpec Allocating data for a Pool giving a PoolSpec and a size Dealing with attribute data arrays on a Pool (TData only)
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4399 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Data/Descriptors/Pool/Pool.hxx
+++ CLAM/src/Data/Descriptors/Pool/Pool.hxx
@@ -2,9 +2,12 @@
 #include <map>
 #include "DataTypes.hxx"
 
+#include "Assert.hxx"
+
 
 namespace CLAM
 {
+	/*
 
 	class CalculationSpec
 	{
@@ -31,17 +34,92 @@ namespace CLAM
 			}
 			return result;
 		}
-		
 	};
 
-/*
 	class Scope
 	{
-		ScopeSpec * _spec;
+		ScopeSpec & _spec;
 	public:
+		Scope(const ScopeSpec & specification) : 
+			_spec(specification) 
+		{
+			
+		}
 	};
 
-*/
+	*/
+
+	class PoolSpec
+	{
+	public:
+		typedef std::map<std::string, unsigned> NamesMap;
+	private:
+		NamesMap _nameMap;
+	public:
+		NamesMap & GetNames()
+		{
+			return _nameMap;
+		}
+
+		unsigned GetIndex(const std::string & name) const
+		{
+			NamesMap::const_iterator it = _nameMap.find(name);
+			CLAM_ASSERT(it!=_nameMap.end(),
+				"Not such descriptor name on this descriptor scope");
+			return it->second;
+		}
+		unsigned GetNAttributes() const
+		{
+			return _nameMap.size();
+		}
+
+		void Add(const std::string & name)
+		{
+			GetNames().insert(std::make_pair(name,GetNames().size()));
+		}
+	};
+
+	class Pool
+	{
+		typedef TData AttributeType;
+		typedef std::vector<AttributeType*> Attributes;
+	
+		unsigned _size;
+		Attributes _attributes;
+		const PoolSpec & _spec;
+	public:
+		Pool(const PoolSpec & spec, unsigned size)
+			: _size(size), _spec(spec)
+		{
+			for (unsigned i = 0; i<size; i++)
+				_attributes.push_back(new AttributeType[_size]);
+		}
+		~Pool()
+		{
+			Attributes::iterator it = _attributes.begin();
+			Attributes::iterator end = _attributes.end();
+			for (; it!=end; it++)
+				delete [] *it;
+		}
+		unsigned GetNAttributes()
+		{
+			return _spec.GetNAttributes();
+		}
+		unsigned GetSize()
+		{
+			return _size;
+		}
+
+		const AttributeType * Get(const std::string & name) const
+		{
+			return _attributes[_spec.GetIndex(name)];
+		}
+		AttributeType * Get(const std::string & name)
+		{
+			return _attributes[_spec.GetIndex(name)];
+		}
+	};
+
 }
 
 
--- CLAM/src/Data/Descriptors/Pool/PoolTest.cxx
+++ CLAM/src/Data/Descriptors/Pool/PoolTest.cxx
@@ -15,8 +15,12 @@ CPPUNIT_TEST_SUITE_REGISTRATION( PoolTest );
 class PoolTest : public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE( PoolTest );
-	CPPUNIT_TEST( testCalculationInserted );
-	CPPUNIT_TEST( testCalculationCalculed );
+	CPPUNIT_TEST( testGetIndex_whenEmpty );
+	CPPUNIT_TEST( testGetIndex_withOneInserted );
+	CPPUNIT_TEST( testGetIndex_withAWrongName );
+	CPPUNIT_TEST( testGetIndex_withSecondInsertedArray );
+	CPPUNIT_TEST( testGet_ReturnsSameMemory );
+	CPPUNIT_TEST( testGet_ReturnsConstMemory );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -26,34 +30,104 @@ public:
 	/// Common clean up, executed after each test method
 	void tearDown() { }
 
+	CLAM::PoolSpec _spec;
+
 private:
 	void assertArrayEquals(unsigned size, CLAM::TData * expected, CLAM::TData * result)
 	{
 		for (unsigned int i = 0; i<size; i++)
 			CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i],result[i],.0001);
+
 	}
 
-	void testCalculationInserted()
+	void testGetIndex_whenEmpty()
 	{
-		CLAM::ScopeSpec spec;
-		CLAM::CalculationSpec calculation;
-		spec.Add("Lala", calculation);
-
-		CPPUNIT_ASSERT_EQUAL(std::string("Lala\n"),spec.DescriptorsList());
+		std::string expected = "Not such descriptor name on this descriptor scope";
+		CLAM::PoolSpec spec;
+		try
+		{
+			spec.GetIndex("Lala");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			CPPUNIT_ASSERT_EQUAL(
+				expected,
+				std::string(err.what()));
+		}
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetNAttributes());
 	}
-	
-	void testCalculationCalculed()
+
+	void testGetIndex_withOneInserted()
 	{
-		CLAM::TData data[10] = { 0,1,-2,3,-4,5,-6,7,-8,9 };
-		CLAM::TData result[10] = { 4,4,4,4,4,4,4,4,4,4 };
-		CLAM::TData expected[10] = { 0,1,2,3,4,5,6,7,8,9 };
-
-//		CLAM::vectorize(CLAM::pow<n>)
-
-	//	assertArraysEquals(10,expected,result);
-
+		CLAM::PoolSpec spec;
+		spec.Add("Lala");
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetIndex("Lala"));
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetNAttributes());
 	}
+
+	void testGetIndex_withAWrongName()
+	{
+		std::string expected = "Not such descriptor name on this descriptor scope";
+		CLAM::PoolSpec spec;
+		spec.Add("Lala");
+		try
+		{
+			spec.GetIndex("Foo");
+			CPPUNIT_FAIL("Should have thrown an exception");
+		}
+		catch (CLAM::ErrAssertionFailed & err)
+		{
+			CPPUNIT_ASSERT_EQUAL(
+				expected,
+				std::string(err.what()));
+		}
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetNAttributes());
+	}
+
+	void testGetIndex_withSecondInsertedArray()
+	{
+		CLAM::PoolSpec spec;
+		spec.Add("Lala");
+		spec.Add("Foo");
+		CPPUNIT_ASSERT_EQUAL(0u,spec.GetIndex("Lala"));
+		CPPUNIT_ASSERT_EQUAL(1u,spec.GetIndex("Foo"));
+		CPPUNIT_ASSERT_EQUAL(2u,spec.GetNAttributes());
+	}
+
+	void testGet_ReturnsSameMemory()
+	{
+		const unsigned poolSize=5;
+		CLAM::PoolSpec spec;
+		spec.Add("Lala");
+		
+		CLAM::Pool pool(spec,poolSize);
+		CLAM::TData * data = pool.Get("Lala");
+		for (unsigned i = 0; i < poolSize; i++)
+			data[i] = i*i;
+		CLAM::TData * data2 = pool.Get("Lala");
+		CPPUNIT_ASSERT_EQUAL(data,data2);
+	}
+
+	void testGet_ReturnsConstMemory()
+	{
+		const unsigned poolSize=5;
+		CLAM::PoolSpec spec;
+		spec.Add("Lala");
+
+		CLAM::Pool pool(spec,poolSize);
+		CLAM::TData * data = pool.Get("Lala");
+		for (unsigned i = 0; i < poolSize; i++)
+			data[i] = i*i;
+		const CLAM::Pool & pool2 = pool;
+		const CLAM::TData * data2 = pool2.Get("Lala");
+		CPPUNIT_ASSERT_EQUAL(const_cast<const CLAM::TData*>(data),data2);
+	}
+
 	
+
+
+
 };
 
 
