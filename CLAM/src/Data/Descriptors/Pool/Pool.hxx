d2aa05b8e6a8b77d2dacd41952e3e6b47a914977
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-04 10:28:55 +0000

    The old quoted code is useless now
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4406 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Data/Descriptors/Pool/Pool.hxx
+++ CLAM/src/Data/Descriptors/Pool/Pool.hxx
@@ -7,48 +7,6 @@
 
 namespace CLAM
 {
-	/*
-
-	class CalculationSpec
-	{
-	};
-
-	class ScopeSpec
-	{
-		typedef std::map<std::string, CalculationSpec> CalculationsMap;
-		CalculationsMap _calculations;
-	public:
-		void Add(const std::string & name, const CalculationSpec & calculation)
-		{
-			_calculations.insert(make_pair(name,calculation));
-		}
-		std::string DescriptorsList()
-		{
-			std::string result;
-			CalculationsMap::iterator it = _calculations.begin();
-			CalculationsMap::iterator end = _calculations.end();
-			for (; it!=end; it++)
-			{
-				result += it->first;
-				result += '\n';
-			}
-			return result;
-		}
-	};
-
-	class Scope
-	{
-		ScopeSpec & _spec;
-	public:
-		Scope(const ScopeSpec & specification) : 
-			_spec(specification) 
-		{
-			
-		}
-	};
-
-	*/
-
 	class PoolSpec
 	{
 	public:
