e4e2f17bbc6fb68e3d17a5c1cd80d868380d914d
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-04 16:46:33 +0000

    almost finished
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4414 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/UnitTests/NodesExperiments/MiniCppUnit2cppUnit.py
+++ CLAM/test/UnitTests/NodesExperiments/MiniCppUnit2cppUnit.py
@@ -7,7 +7,6 @@ if len(sys.argv) == 1 :
 	sys.exit(1)
 	
 print 'processing file:', sys.argv[1]
-fullText = ''
 
 contains = lambda line, search : line.find(search)>=0
 
@@ -15,28 +14,54 @@ substitutions = [
 	('ASSERT_IGUALS', 'CPPUNIT_ASSERT_EQUAL'),
 	('#include\s*"MiniCppUnit.hxx"', '#include <cppunit/extensions/HelperMacros.h>'),
 	('public GrupDeTests<\S+>','public CppUnit::TestFixture' ),
-	(r'\tCAS_DE_TEST', 'CPPUNIT_TEST' )
+	('\tCAS_DE_TEST', 'CPPUNIT_TEST' ),
+	('GRUP_DE_TESTS','CPPUNIT_TEST_SUITE'),
+	('FALLA', 'CPPUNIT_FAIL' )
 	]
 
 parsingState = 'DEFAULT'
+
+
+def checkAsserts(aLine):
+	assertPattern = 'ASSERT\((.*)' 
+	matchAssert = re.search(assertPattern, aLine)	
+	if matchAssert:
+		cppunitAssertPattern = 'CPPUNIT_ASSERT('+matchAssert.group(1)
+		aLine = re.sub( assertPattern, cppunitAssertPattern, aLine )
+	return aLine
+	
 def processLine(aLine) :
 	global parsingState
 
 	if parsingState == 'DEFAULT' :
 		if contains(aLine, 'GRUP_DE_TESTS') :
+			matchGrup = re.search('GRUP_DE_TESTS\(\s*(\S+)\s*\)', aLine )
+			aLine = re.sub( 'GRUP_DE_TESTS\(\s*(\S+)\s*\)', 'CPPUNIT_TEST_SUITE( '+matchGrup.group(1) + ' );', aLine )
 			parsingState = 'TESTS_DECL'
+		if contains(aLine, 'int main'):
+			parsingState = 'MAIN'
+			aLine = ''
+			
 	if parsingState == 'TESTS_DECL' :
 		if contains(aLine, '{') :
-			aLine = ''
+			aLine = '\n'
 		if contains(aLine, '}') :
-			aLine = '\tCPPUNIT_TEST_SUITE_END();'
-			
+			aLine = '\n\tCPPUNIT_TEST_SUITE_END();\n'
+			parsingState = 'DEFAULT'
+
+	if parsingState == 'MAIN' :		
+		if contains(aLine,'}') :
+			parsingState = 'DEFAULT'
+		aLine = ''
+	
 	changed = False
 	for pattern, subst in substitutions :
 		match = re.search( pattern, aLine)
 		changed = changed or match
 		if match:
 			aLine = re.sub( pattern, subst, aLine )
+
+	aLine = checkAsserts( aLine )
 	
 	return changed, aLine
 
@@ -47,12 +72,4 @@ for line in file(sys.argv[1]) :
 	else :
 		print '···',
 	print processedLine,
-	fullText += processedLine
-
-	
-#matchFoo = re.search( 'GRUP_DE_TESTS\(\s*(\S+)\s*\)\n\s*\{*\}', fullText )
-#matchFinalTestDeclaration = re.search( 'CPPUNIT_TEST\(\s*(\S*)\s*\);\s*\n\s*}', fullText ) 
-#fullText = re.sub( 'CPPUNIT_TEST\(\s*(\S+)\s*\);\s*\n\s*}', 'CPPUNIT_TEST( ' + matchFinalTestDeclaration.group(1)+' );\n\t\tCPPUNIT_TEST_SUITE_END();\n\t}\n', fullText )
-#print 'this is the full text',fullText
-#print 'match foo:',matchFoo.group(1)
 
