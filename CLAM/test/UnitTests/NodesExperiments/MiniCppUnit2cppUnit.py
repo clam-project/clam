a6f04efd649b2c2c48a535e646e3d311c2ae9701
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-04 16:04:02 +0000

    a mig fer
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4412 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/UnitTests/NodesExperiments/MiniCppUnit2cppUnit.py
+++ CLAM/test/UnitTests/NodesExperiments/MiniCppUnit2cppUnit.py
@@ -9,20 +9,35 @@ if len(sys.argv) == 1 :
 print 'processing file:', sys.argv[1]
 fullText = ''
 
+contains = lambda line, search : line.find(search)>=0
+
 substitutions = [
 	('ASSERT_IGUALS', 'CPPUNIT_ASSERT_EQUAL'),
 	('#include\s*"MiniCppUnit.hxx"', '#include <cppunit/extensions/HelperMacros.h>'),
 	('public GrupDeTests<\S+>','public CppUnit::TestFixture' ),
-	('CAS_DE_TEST', 'CPPUNIT_TEST' )
+	(r'\tCAS_DE_TEST', 'CPPUNIT_TEST' )
 	]
 
+parsingState = 'DEFAULT'
 def processLine(aLine) :
+	global parsingState
+
+	if parsingState == 'DEFAULT' :
+		if contains(aLine, 'GRUP_DE_TESTS') :
+			parsingState = 'TESTS_DECL'
+	if parsingState == 'TESTS_DECL' :
+		if contains(aLine, '{') :
+			aLine = ''
+		if contains(aLine, '}') :
+			aLine = '\tCPPUNIT_TEST_SUITE_END();'
+			
 	changed = False
 	for pattern, subst in substitutions :
 		match = re.search( pattern, aLine)
 		changed = changed or match
 		if match:
 			aLine = re.sub( pattern, subst, aLine )
+	
 	return changed, aLine
 
 for line in file(sys.argv[1]) :
@@ -35,9 +50,9 @@ for line in file(sys.argv[1]) :
 	fullText += processedLine
 
 	
-matchFoo = re.search( 'GRUP_DE_TESTS\(\s*(\S+)\s*\)\n\s*\{*\}', fullText )
+#matchFoo = re.search( 'GRUP_DE_TESTS\(\s*(\S+)\s*\)\n\s*\{*\}', fullText )
 #matchFinalTestDeclaration = re.search( 'CPPUNIT_TEST\(\s*(\S*)\s*\);\s*\n\s*}', fullText ) 
 #fullText = re.sub( 'CPPUNIT_TEST\(\s*(\S+)\s*\);\s*\n\s*}', 'CPPUNIT_TEST( ' + matchFinalTestDeclaration.group(1)+' );\n\t\tCPPUNIT_TEST_SUITE_END();\n\t}\n', fullText )
-print 'this is the full text',fullText
-print 'match foo:',matchFoo.group(1)
+#print 'this is the full text',fullText
+#print 'match foo:',matchFoo.group(1)
 
