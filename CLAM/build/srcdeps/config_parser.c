3012178c90cd7e595caf37262d2717ca69e39c9a
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-08 16:10:40 +0000

    + Added the entry "MOCABLE_HEADERS" to the 'config' map so we can store the headers with the Q_OBJECT thing present
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4446 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/build/srcdeps/config_parser.c
+++ CLAM/build/srcdeps/config_parser.c
@@ -24,6 +24,9 @@ list *cxxflags_release = 0;
 
 list *library_paths = 0;
 
+list *mocable_headers = NULL;
+list *mocable_sources = NULL;
+
 list *program = 0;
 
 static int equalsigncount = 0;
@@ -498,6 +501,17 @@ void config_init(void)
 	cxxflags_release = 
 		listhash_add_key_once(config,"CXXFLAGS_RELEASE")->l = list_new();
 	list_lock(cxxflags_release );
+
+	mocable_headers = 
+		listhash_add_key_once(config,"MOCABLE_HEADERS")->l = list_new();
+
+	list_lock( mocable_headers );
+
+	mocable_sources =
+		listhash_add_key_once(config,"MOCABLE_SOURCES")->l = list_new();
+
+	list_lock( mocable_sources );
+
 	
 	program = 
 		listhash_add_key_once(config,"PROGRAM")->l = list_new();
@@ -508,6 +522,7 @@ void config_init(void)
 	listhash_add_key_once(config,"DEFINES")->l = list_new();
 	listhash_add_key_once(config,"SEARCH_INCLUDES")->l = list_new();
 	listhash_add_key_once(config,"SEARCH_RECURSE_INCLUDES")->l = list_new();
+
 
 	list_add_str_once(used_vars,"LIBRARIES_DEBUG");
 	list_add_str_once(used_vars,"LIBRARIES_RELEASE");
@@ -567,6 +582,8 @@ void config_exit(void)
 	list_unlock(link_flags_release );
 	list_unlock(cxxflags_debug );
 	list_unlock(cxxflags_release );
+	list_unlock(mocable_headers );
+	list_unlock(mocable_sources );
 	list_unlock(program );
 	
 	listhash_free(config);
