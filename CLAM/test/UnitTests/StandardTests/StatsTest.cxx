63976a04b6388931fb4013bf6438e5509e7c3ed0
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-18 00:25:43 +0000

    Tilt, Slope and creation helpers
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4578 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/UnitTests/StandardTests/StatsTest.cxx
+++ CLAM/test/UnitTests/StandardTests/StatsTest.cxx
@@ -21,61 +21,136 @@ class StatsTest : public CppUnit::TestFixture
 	CPPUNIT_TEST( testMoment4 );
 	CPPUNIT_TEST( testMoment5 );
 	CPPUNIT_TEST( testMoment6 );
+	CPPUNIT_TEST( testSlope_withPrimesUp );
+	CPPUNIT_TEST( testSlope_withPrimesDown );
+	CPPUNIT_TEST( testSlope_withConstant );
+	CPPUNIT_TEST( testTilt_withPrimesUp );
+	CPPUNIT_TEST( testTilt_withPrimesDown );
+	CPPUNIT_TEST( testTilt_withConstant );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
 	/// Common initialization, executed before each test method
 	void setUp() 
 	{
-		_primeArray.AddElem(1.);
-		_primeArray.AddElem(2.);
-		_primeArray.AddElem(3.);
-		_primeArray.AddElem(5.);
 	}
 
 	/// Common clean up, executed after each test method
-	void tearDown() { }
+	void tearDown()
+	{
+	}
+	
+private:
+	void fillPrimesUp()
+	{
+		_array.AddElem(1.);
+		_array.AddElem(2.);
+		_array.AddElem(3.);
+		_array.AddElem(5.);
+	}
+	void fillConstant()
+	{
+		_array.AddElem(3.);
+		_array.AddElem(3.);
+		_array.AddElem(3.);
+		_array.AddElem(3.);
+	}
+
+	void fillPrimesDown()
+	{
+		_array.AddElem(5.);
+		_array.AddElem(3.);
+		_array.AddElem(2.);
+		_array.AddElem(1.);
+	}
+
+	CLAM::Array<CLAM::TData> _array;
 
 private:
-
-		CLAM::Array<CLAM::TData> _primeArray;
-
 	void testMoment1()
 	{
+		fillPrimesUp();
 
-		CLAM::Stats stats(&_primeArray);
+		CLAM::Stats stats(&_array);
 
 		CPPUNIT_ASSERT_EQUAL( CLAM::TData(2.75), stats.GetMoment((CLAM::O<1>*)0));
 	}
 	void testMoment2()
 	{
-		CLAM::Stats stats(&_primeArray);
+		fillPrimesUp();
+
+		CLAM::Stats stats(&_array);
 
 		CPPUNIT_ASSERT_EQUAL( CLAM::TData(9.75), stats.GetMoment((CLAM::O<2>*)0));
 	}
 	void testMoment3()
 	{
-		CLAM::Stats stats(&_primeArray);
+		fillPrimesUp();
+
+		CLAM::Stats stats(&_array);
 
 		CPPUNIT_ASSERT_EQUAL( CLAM::TData(40.25), stats.GetMoment((CLAM::O<3>*)0));
 	}
 	void testMoment4()
 	{
-		CLAM::Stats stats(&_primeArray);
+		fillPrimesUp();
+
+		CLAM::Stats stats(&_array);
 
 		CPPUNIT_ASSERT_EQUAL( CLAM::TData(180.75), stats.GetMoment((CLAM::O<4>*)0));
 	}
 	void testMoment5()
 	{
-		CLAM::Stats stats(&_primeArray);
+		fillPrimesUp();
+
+		CLAM::Stats stats(&_array);
 
 		CPPUNIT_ASSERT_EQUAL( CLAM::TData(850.25), stats.GetMoment((CLAM::O<5>*)0));
 	}
 	void testMoment6()
 	{
-		CLAM::Stats stats(&_primeArray);
+		fillPrimesUp();
+
+		CLAM::Stats stats(&_array);
 
 		CPPUNIT_ASSERT_EQUAL( CLAM::TData(4104.75), stats.GetMoment((CLAM::O<6>*)0));
+	}
+	void testSlope_withPrimesUp()
+	{
+		fillPrimesUp();
+		CLAM::Stats stats(&_array);
+		CPPUNIT_ASSERT_DOUBLES_EQUAL( CLAM::TData(0.118182), stats.GetSlope() , .000001);
+	}
+	void testSlope_withPrimesDown()
+	{
+		fillPrimesDown();
+		CLAM::Stats stats(&_array);
+
+		CPPUNIT_ASSERT_DOUBLES_EQUAL( CLAM::TData(-0.118182), stats.GetSlope() , .000001);
+	}
+	void testSlope_withConstant()
+	{
+		fillConstant();
+		CLAM::Stats stats(&_array);
+		CPPUNIT_ASSERT_EQUAL( CLAM::TData(0.), stats.GetSlope());
+	}
+	void testTilt_withPrimesUp()
+	{
+		fillPrimesUp();
+		CLAM::Stats stats(&_array);
+		CPPUNIT_ASSERT_DOUBLES_EQUAL( CLAM::TData(0.118182), stats.GetTilt() , .000001);
+	}
+	void testTilt_withPrimesDown()
+	{
+		fillPrimesDown();
+		CLAM::Stats stats(&_array);
+		CPPUNIT_ASSERT_DOUBLES_EQUAL( CLAM::TData(-0.118182), stats.GetTilt() , .000001);
+	}
+	void testTilt_withConstant()
+	{
+		fillConstant();
+		CLAM::Stats stats(&_array);
+		CPPUNIT_ASSERT_EQUAL( CLAM::TData(0.), stats.GetTilt());
 	}
 };
 
