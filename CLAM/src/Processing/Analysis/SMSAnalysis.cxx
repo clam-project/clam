4979f3df032842f39f1d23c91e1afb858797c7c7
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-26 10:32:29 +0000

    +removed unused variables
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4644 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/Analysis/SMSAnalysis.cxx
+++ CLAM/src/Processing/Analysis/SMSAnalysis.cxx
@@ -464,8 +464,6 @@ bool SMSAnalysis::Do(Segment& in)
 //first we compute necessary sizes, indices and parameters
 	int frameIndex=in.mCurrentFrameIndex;
 	int step=mConfig.GetHopSize();
-	int sinFrameSize=mConfig.GetSinSpectralAnalysis().GetWindowSize()-1;
-	int resFrameSize=mConfig.GetResSpectralAnalysis().GetWindowSize()-1;
 	TData samplingRate=mConfig.GetSamplingRate();
 	TTime frameCenterTime=frameIndex*step/samplingRate;
 	//Audio center time is different from frame center time. This index corresponds to
