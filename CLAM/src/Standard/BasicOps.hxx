2dec5410c80a24d7383af125fec014186ce52cb5
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-03-30 10:19:53 +0000

    Doxygen comments were applied to the namespace
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4658 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Standard/BasicOps.hxx
+++ CLAM/src/Standard/BasicOps.hxx
@@ -15,20 +15,21 @@ using std::inner_product;
 using std::mem_fun;
 
 
-/** @todo: 
+/** 
+ @file BasicOps.cxx
+ @todo 
 - There a lot of hacks because of Visual C++ compiler, could be optimized and cleaned-up
 	making use of partial specialization. 
 - It is still not clear whether the "abs" aspect should be promoted later on to the argument
 	list.
-*/
+ * Note: the "abs" aspect present in many of the operations means whether the statistic should
+ *	be performed directly on the values (by default or when abs=false) or whether the statistic
+ *	should be computed on the absolute value
+ */
 
 namespace CLAM {
 
 
-/** Note: the "abs" aspect present in many of the operations means whether the statistic should
- *	be performed directly on the values (by default or when abs=false) or whether the statistic
- *	should be computed on the absolute value
- */
 
 namespace Implementation {}
 
