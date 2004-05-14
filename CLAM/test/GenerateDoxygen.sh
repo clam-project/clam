721e5186ebb8eb61421eb20761545f4e18e26644
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-05-14 17:52:11 +0000

    Uploading a tarball Some removal problems solved The devel version is tagged with the date
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4879 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/GenerateDoxygen.sh
+++ CLAM/test/GenerateDoxygen.sh
@@ -7,18 +7,27 @@ GenerateDoxygen()
 	Name=$1
 	Branch=$2
 	TargetDir=$3
+	Date=$4
 	cvs co $Branch -d CLAM-for-doxygen CLAM
 	pushd CLAM-for-doxygen
-	# VERSION=`grep PROJECT_NUMBER .doxygen.cfg | (read var eq value; echo $value )`
-	echo Generating Doxygen, output will be on the DoxyLog file...
-	(
-		cat .doxygen.cfg
-		echo "PROJECT_NAME = $Name"
-	) | doxygen - >DoxyLog 2>&1 
+		Version=`grep PROJECT_NUMBER .doxygen.cfg | (read var eq value; echo $value )`
+		echo Generating Doxygen, output will be on the DoxyLog file...
+		(
+			cat .doxygen.cfg
+			echo "PROJECT_NAME = $Name"
+			echo "PROJECT_NUMBER = $Version $Date"
+		) | doxygen - >DoxyLog 2>&1 
 
-	ssh clamadm@www.iua.upf.es mv $TargetDir /tmp
-	scp -r doxygen/html "clamadm@www.iua.upf.es:$TargetDir"
-	scp -r DoxyLog "clamadm@www.iua.upf.es:$TargetDir"
+		Tarball=$TargetDir.tar.bz
+
+		mv doxygen/html $TargetDir
+		tar cfvj $Tarball $TargetDir
+
+		ssh clamadm@www.iua.upf.es rm -rf $TargetDir
+		scp -r $Tarball "clamadm@www.iua.upf.es:"
+		ssh clamadm@www.iua.upf.es tar xfvj $Tarball
+		ssh clamadm@www.iua.upf.es rm $Tarball
+		scp -r DoxyLog "clamadm@www.iua.upf.es:$TargetDir"
 	popd
 	rm -rf CLAM-for-doxygen
 }
@@ -26,10 +35,11 @@ GenerateDoxygen()
 
 if [ "$1" == "main" ]
 then
-	GenerateDoxygen "CLAM" "" "CLAM-doxygen"
+	GenerateDoxygen "CLAM" "" "CLAM-doxygen" ""
 elif [ "$1" == "devel" ]
 then
-	GenerateDoxygen "CLAM-development" "-r development-branch" "CLAM-devel-doxygen"
+	Date=`date -u +"%Y-%m-%d"`
+	GenerateDoxygen "CLAM-development" "-r development-branch" "CLAM-devel-doxygen" "__$Date"
 else
 	echo "For main branch:  $0 main "  >&2
 	echo "For devel branch:  $0 devel "  >&2
