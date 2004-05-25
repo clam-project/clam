15271f866462437882cf4ea6b6fd41a9a2eb43bf
xrubio <xrubio@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-05-25 14:02:04 +0000

    removed SMSAnalysiisConfig code
    git-svn-id: https://clam-project.org/clam/branches/CLAM-NewPorts-branch@4914 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/Analysis/SMSAnalysisCore.cxx
+++ CLAM/src/Processing/Analysis/SMSAnalysisCore.cxx
@@ -28,168 +28,13 @@
 namespace CLAM
 {
 
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-/*					SMSANALYSIS CONFIGURATION						*/
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-
-
-
-void SMSAnalysisConfig::DefaultInit()
-{
-	AddAll();
-	UpdateData();
-	DefaultValues();	
-}
-
-void SMSAnalysisConfig::DefaultValues()
-{
-	SetprSamplingRate(44100);
-
-	GetSinSpectralAnalysis().SetWindowType(EWindowType::eHamming);
-	GetResSpectralAnalysis().SetWindowType(EWindowType::eBlackmanHarris92);
-
-	GetPeakDetect().SetMagThreshold(-60);
-	
-}
-
-
-void SMSAnalysisConfig::SetSinWindowSize(TSize w)
-{
-	GetSinSpectralAnalysis().SetWindowSize(w);
-}
-
-TSize SMSAnalysisConfig::GetSinWindowSize() const
-{
-	return GetSinSpectralAnalysis().GetWindowSize();
-}
-
-/** Analysis Window type*/
-void SMSAnalysisConfig::SetSinWindowType(const EWindowType& t)
-{
-	GetSinSpectralAnalysis().SetWindowType(t);
-}
-
-const EWindowType& SMSAnalysisConfig::GetSinWindowType() const
-{
-	return GetSinSpectralAnalysis().GetWindowType();
-}
-
-/** Zero padding factor*/
-void SMSAnalysisConfig::SetSinZeroPadding(int z)
-{
-	GetSinSpectralAnalysis().SetZeroPadding(z);
-}
-
-int SMSAnalysisConfig::GetSinZeroPadding() const
-{
-	return GetSinSpectralAnalysis().GetZeroPadding();
-}
-
-void SMSAnalysisConfig::SetHopSize(TSize h)
-{
-	GetSinSpectralAnalysis().SetHopSize(h);
-	GetResSpectralAnalysis().SetHopSize(h);
-}
-
-TSize SMSAnalysisConfig::GetHopSize() const
-{
-	return GetSinSpectralAnalysis().GetHopSize();
-}
-
-/*****Configuration for residual component analysis*****/
-
-void SMSAnalysisConfig::SetResWindowSize(TSize w)
-{
-	GetResSpectralAnalysis().SetWindowSize(w);
-	GetSynthSineSpectrum().SetSpectrumSize(GetResSpectralAnalysis().GetFFT().GetAudioSize()/2+1);
-}
-
-TSize SMSAnalysisConfig::GetResWindowSize() const
-{
-	return GetResSpectralAnalysis().GetWindowSize();
-}
-
-/** Analysis Window type*/
-void SMSAnalysisConfig::SetResWindowType(const EWindowType& t)
-{
-	GetResSpectralAnalysis().SetWindowType(t);
-}
-
-const EWindowType& SMSAnalysisConfig::GetResWindowType() const
-{
-	return GetResSpectralAnalysis().GetWindowType();
-}
-
-/** Zero padding factor*/
-void SMSAnalysisConfig::SetResZeroPadding(int z)
-{
-	GetResSpectralAnalysis().SetZeroPadding(z);
-}
-
-int SMSAnalysisConfig::GetResZeroPadding() const
-{
-	return GetResSpectralAnalysis().GetZeroPadding();
-}
-
-/** Sampling rate of the input audio*/
-void SMSAnalysisConfig::SetSamplingRate(TData sr)
-{
-	SetprSamplingRate(sr);
-	GetSinSpectralAnalysis().SetSamplingRate(sr);
-	GetResSpectralAnalysis().SetSamplingRate(sr);
-	GetSynthSineSpectrum().SetSamplingRate(sr);
-}
-
-TData SMSAnalysisConfig::GetSamplingRate() const
-{
-	return TData(GetprSamplingRate());
-}
-
-
-TInt32 SMSAnalysisConfig::PowerOfTwo(TInt32 size)
-{
-	int tmp = size;
-	int outputSize = 1;
-	while (tmp) 
-	{
-	 	outputSize=outputSize << 1;
-	 	tmp=tmp >> 1;
-	}
-	if(outputSize == size << 1)
-		outputSize = outputSize >> 1;
-	return outputSize;
-}
-
-TSize SMSAnalysisConfig::GetInitialOffset() const
-{
-	TSize largerWindowSize;
-	if(GetSinWindowSize()>GetResWindowSize()) largerWindowSize=GetSinWindowSize();
-	else largerWindowSize=GetResWindowSize();
-			
-	return -(largerWindowSize-1)/2+GetHopSize();
-}
-
-TSize SMSAnalysisConfig::GetHopsInBiggerWindow() const
-{
-	if(GetSinWindowSize()>GetResWindowSize()) return (GetSinWindowSize()-1)/GetHopSize();
-	else return (GetResWindowSize()-1)/GetHopSize();
-}
-
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-/*							SMSANALYSIS 							*/
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-
 SMSAnalysisCore::SMSAnalysisCore()
 	: mInputAudio("Input Audio",this ),
-	mOutputResSpectrum("Residual Spectral Analysis",this ),
-	mOutputSinSpectrum("Sinusoidal Spectral Analysis",this ),
-	mOutputSpectralPeaks("Sinusoidal Tracking",this ),
+	mOutputResSpectrum("Residual Branch Spectrum",this ),
+	mOutputSinSpectrum("Sinusoidal Branch Spectrum",this ),
+	mOutputSpectralPeaks("Sinusoidal Peaks",this ),
 	mOutputFundamental("Fundamental",this ),
-	mOutputSubstractedSpectrum("Substracted Spectral Analysis", this )
+	mOutputSubstractedSpectrum("Residual Spectrum", this )
 {
 	AttachChildren();
 	ConnectAndPublishPorts();
@@ -198,11 +43,11 @@ SMSAnalysisCore::SMSAnalysisCore()
 
 SMSAnalysisCore::SMSAnalysisCore(SMSAnalysisConfig& cfg)
 	: mInputAudio("Input Audio",this ),
-	mOutputResSpectrum("Residual Spectral Analysis",this ),
-	mOutputSinSpectrum("Sinusoidal Spectral Analysis",this ),
-	mOutputSpectralPeaks("Sinusoidal Tracking",this ),
+	mOutputResSpectrum("Residual Branch Spectrum",this ),
+	mOutputSinSpectrum("Sinusoidal Branch Spectrum",this ),
+	mOutputSpectralPeaks("Sinusoidal Peaks",this ),
 	mOutputFundamental("Fundamental",this ),
-	mOutputSubstractedSpectrum("Substracted Spectral Analysis", this )
+	mOutputSubstractedSpectrum("Residual Spectrum", this )
 {
 	AttachChildren();
 	ConnectAndPublishPorts();
@@ -318,7 +163,8 @@ bool SMSAnalysisCore::Do()
 	{
 		mSpecSubstracter.Do();
 	}
-
+	
+	return true;
 }
 
 void SMSAnalysisCore::ConnectAndPublishPorts()
