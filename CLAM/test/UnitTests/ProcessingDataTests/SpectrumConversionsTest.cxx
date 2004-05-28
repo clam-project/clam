ba181ff93d636d62839dd354ee3a80d573b62809
xamat <xamat@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-05-28 16:46:25 +0000

    Added a couple of tests, but still are passing.
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4943 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/UnitTests/ProcessingDataTests/SpectrumConversionsTest.cxx
+++ CLAM/test/UnitTests/ProcessingDataTests/SpectrumConversionsTest.cxx
@@ -17,6 +17,7 @@ class SpectrumConversionsTest : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE( SpectrumConversionsTest );
 	CPPUNIT_TEST( testDefaultSpectrum );
 	CPPUNIT_TEST( testMagPhase2BPF2MagPhase );
+	CPPUNIT_TEST( testComplex2BPF2Complex );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -62,23 +63,113 @@ private:
 			phase[i] = CLAM::TData(-0.1);
 		}
 		CLAM::SpecTypeFlags bpfAndMagphaseFlag;
-		spec.SetType(bpfAndMagphaseFlag);
+		bpfAndMagphaseFlag.bMagPhaseBPF = true;
+		bpfAndMagphaseFlag.bMagPhase=false;
+		spec.SetTypeSynchronize(bpfAndMagphaseFlag);
 
 		CLAM::SpecTypeFlags magphaseFlag;
-		magphaseFlag.bMagPhaseBPF = true;
-		spec.SynchronizeTo(magphaseFlag);
+	
 		CPPUNIT_ASSERT_EQUAL(22050, spec.GetBPFSize() );
 
 		CLAM::BPF & magBpf = spec.GetMagBPF();
 		CLAM::BPF & phaseBpf = spec.GetPhaseBPF();
 		for(int i=0; i<22050; i++)
 		{
-			CPPUNIT_ASSERT( CLAM::TData(0.1) == magBpf.GetXValue(i) );
-			CPPUNIT_ASSERT( CLAM::TData(-0.1) == phaseBpf.GetXValue(i) );
+			CPPUNIT_ASSERT( CLAM::TData(0.1) == magBpf.GetValue(i) );
+			CPPUNIT_ASSERT( CLAM::TData(-0.1) == phaseBpf.GetValue(i) );
 		}
-
+		spec.SetTypeSynchronize(magphaseFlag);
+		for(int i=0; i<22050; i++)
+		{
+			CPPUNIT_ASSERT( mag[i] == CLAM::TData(0.1));
+			CPPUNIT_ASSERT( phase[i] == CLAM::TData(-0.1));
+		}
 	}
+	void testComplex2BPF2Complex()
+	{	
+		CLAM::Spectrum spec;
+		CLAM::SpectrumConfig config;
+		config.SetSize(22050);
+		CLAM::SpecTypeFlags complexFlag;
+		complexFlag.bMagPhase=false;
+		complexFlag.bComplex=true;
+		config.SetType(complexFlag);
+		spec.Configure(config);
 
+		CLAM::Array<CLAM::Complex> & complexArray = spec.GetComplexArray();
+		for(int i=0; i<22050; i++)
+		{
+			complexArray[i].SetReal(CLAM::TData(1));
+			complexArray[i].SetImag(CLAM::TData(0));
+		}
+		CLAM::SpecTypeFlags bpfFlag;
+		bpfFlag.bMagPhase=false;
+		bpfFlag.bMagPhaseBPF = true;
+		spec.SetTypeSynchronize(bpfFlag);
+
+			
+		CPPUNIT_ASSERT_EQUAL(22050, spec.GetBPFSize() );
+
+		CLAM::BPF & magBpf = spec.GetMagBPF();
+		CLAM::BPF & phaseBpf = spec.GetPhaseBPF();
+		for(int i=0; i<22050; i++)
+		{
+			CPPUNIT_ASSERT( CLAM::TData(1) == magBpf.GetValue(i) );
+			CPPUNIT_ASSERT( CLAM::TData(0) == phaseBpf.GetValue(i) );
+		}
+		spec.SetTypeSynchronize(complexFlag);
+		for(int i=0; i<22050; i++)
+		{
+			CPPUNIT_ASSERT( complexArray[i].Real() == CLAM::TData(1));
+			CPPUNIT_ASSERT( complexArray[i].Imag() == CLAM::TData(0));
+		}
+	}	
+	void testComplex2BPF2ComplexWithDifferentSpectrums()
+	{	
+		CLAM::Spectrum complexSpec;
+		CLAM::SpectrumConfig config;
+		config.SetSize(22050);
+		CLAM::SpecTypeFlags complexFlag;
+		complexFlag.bMagPhase=false;
+		complexFlag.bComplex=true;
+		config.SetType(complexFlag);
+		complexSpec.Configure(config);
+
+		CLAM::Spectrum BPFSpec;
+		CLAM::SpectrumConfig config2;
+		config2.SetSize(22050);
+		CLAM::SpecTypeFlags bpfFlag;
+		bpfFlag.bMagPhase=false;
+		bpfFlag.bMagPhaseBPF=true;
+		config2.SetType(bpfFlag);
+		BPFSpec.Configure(config2);
+
+		CLAM::Array<CLAM::Complex> & complexArray = complexSpec.GetComplexArray();
+		for(int i=0; i<22050; i++)
+		{
+			complexArray[i].SetReal(CLAM::TData(1));
+			complexArray[i].SetImag(CLAM::TData(0));
+		}
+	
+		BPFSpec.SynchronizeTo(complexSpec);
+
+			
+		CPPUNIT_ASSERT_EQUAL(22050, complexSpec.GetBPFSize() );
+
+		CLAM::BPF & magBpf = BPFSpec.GetMagBPF();
+		CLAM::BPF & phaseBpf = BPFSpec.GetPhaseBPF();
+		for(int i=0; i<22050; i++)
+		{
+			CPPUNIT_ASSERT( CLAM::TData(1) == magBpf.GetValue(i) );
+			CPPUNIT_ASSERT( CLAM::TData(0) == phaseBpf.GetValue(i) );
+		}
+		complexSpec.SynchronizeTo(BPFSpec);
+		for(int i=0; i<22050; i++)
+		{
+			CPPUNIT_ASSERT( complexArray[i].Real() == CLAM::TData(1));
+			CPPUNIT_ASSERT( complexArray[i].Imag() == CLAM::TData(0));
+		}
+	}	
 	void testMagPhase2Complex2MagPhase()
 	{
 
