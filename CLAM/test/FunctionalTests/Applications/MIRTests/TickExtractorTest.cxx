d07b1bd916faf9f982baeae38608cb881238fea5
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-06-08 08:39:43 +0000

    + All "test files" now are covered by test cases
    git-svn-id: https://clam-project.org/clam/branches/development-branch@4988 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/FunctionalTests/Applications/MIRTests/TickExtractorTest.cxx
+++ CLAM/test/FunctionalTests/Applications/MIRTests/TickExtractorTest.cxx
@@ -16,7 +16,17 @@ namespace CLAMTest
 
 		CPPUNIT_TEST( test_02_alles_Ticks );
 		CPPUNIT_TEST( test_12_cubic_Ticks );
+		CPPUNIT_TEST( test_ALoCubano_Ticks );
+		CPPUNIT_TEST( test_AmourEnPoudre_Ticks );
+		CPPUNIT_TEST( test_Amsterdam_Ticks );
 		CPPUNIT_TEST( test_blue_monday_Ticks );
+		CPPUNIT_TEST( test_clicseq_Ticks );
+		CPPUNIT_TEST( test_drums_electronic_guit_Ticks );
+		CPPUNIT_TEST( test_elecpiano_drums_Ticks );
+		CPPUNIT_TEST( test_test1_Ticks );
+		CPPUNIT_TEST( test_test2_Ticks );
+		CPPUNIT_TEST( test_test_Ticks );
+
 
 		CPPUNIT_TEST_SUITE_END();
 
@@ -47,10 +57,12 @@ namespace CLAMTest
 						inst.IsValid() == true);
 
 			CLAM::DataArray extractedEvents;
-			
-			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents );
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
 
 			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
 		}
 
 		void test_12_cubic_Ticks()
@@ -63,10 +75,12 @@ namespace CLAMTest
 						 inst.IsValid() == true );
 
 			CLAM::DataArray extractedEvents;
-
-			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents );
+			CLAM::TData     tickRate = 0;
+			
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
 
 			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
 		}
 
 		void test_blue_monday_Ticks()
@@ -79,11 +93,186 @@ namespace CLAMTest
 						inst.IsValid() == true);
 
 			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
 
-			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents );
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
 
 			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
 
 		}
+
+		void test_ALoCubano_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/ALoCubano_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/ALoCubano_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_AmourEnPoudre_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/AmourEnPoudre_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/AmourEnPoudre_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_Amsterdam_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/Amsterdam_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/Amsterdam_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_clicseq_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/clicseq_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/clicseq_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_drums_electronic_guit_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/drums_electronic_guit_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/drums_electronic_guit_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_elecpiano_drums_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/elecpiano_drums_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/elecpiano_drums_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_test1_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/test1_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/test1_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_test2_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/test2_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/test2_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+		void test_test_Ticks()
+		{
+			RythmDescriptionTests::RythmEventsB2B inst;
+
+			CLAM::XMLStorage::Restore( inst, mPathToTests + "/Tick/test_Ticks.xml" );
+
+			CPPUNIT_ASSERT_MESSAGE( "Error loading back to back test data: problem with RythmDescription/Tick/test_Ticks.xml",
+						inst.IsValid() == true);
+
+			CLAM::DataArray extractedEvents;
+			CLAM::TData     tickRate = 0;
+
+			CLAM::ExtractTicksSequence( mPathToTests + "/" + inst.GetRelativePath(), extractedEvents, tickRate );
+
+			CPPUNIT_ASSERT_EQUAL( inst.GetEvents().GetIndexes().Size(), extractedEvents.Size() );
+			CPPUNIT_ASSERT_EQUAL( int(inst.GetEvents().GetRate()), int(tickRate) );
+
+		}
+
+
 	};
 }
