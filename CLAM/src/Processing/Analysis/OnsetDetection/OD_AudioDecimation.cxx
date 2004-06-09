410417170e44f583d2e7fcf2a53f76c84e4b27a9
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-06-09 11:05:33 +0000

    + Refactoring the beast...
    git-svn-id: https://clam-project.org/clam/branches/development-branch@5001 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/Analysis/OnsetDetection/OD_AudioDecimation.cxx
+++ CLAM/src/Processing/Analysis/OnsetDetection/OD_AudioDecimation.cxx
@@ -174,30 +174,41 @@ namespace CLAM
 			b3 = -3.90004183080475;
 			b4 = 0.96711584278936;
 				
-						
+			// The "difficult" part of the filtering loop has been stripmined
+			mOutput[0] = a0*signal[0];
+
+			mOutput[1] = a0*signal[1]+ a1*signal[0]
+				- b1*mOutput[0];
+
+			mOutput[2] = a0*signal[2]+ a1*signal[1] + a2*signal[0] 
+				- b1*mOutput[1] - b2*mOutput[0];
+
+			mOutput[3] = a0*signal[3]+ a1*signal[2] + a2*signal[1] + a3*signal[0] 
+				- b1*mOutput[2] - b2*mOutput[1] - b3*mOutput[0];
+
+			
 			//filtering
-			for(i=0; i<size; i++)
+			for(i=4; i<size; i++)
 			{
-				if(i==0) mOutput[i] = a0*signal[i];
-				else if(i==1) mOutput[i] = a0*signal[i]+ a1*signal[i-1]- b1*mOutput[i-1];
-				else if(i==2) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] - b1*mOutput[i-1] - b2*mOutput[i-2];
-				else if(i==3) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3] - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3];
-				else mOutput[i] = a0*signal[i] + a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3] + a4*signal[i-4] 
-					     - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4];
-				
-				mOutputRev[size-1-i] = mOutput[i];
+				mOutput[i] = a0*signal[i] + a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3] + a4*signal[i-4] 
+					     - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4];				
 			}
 
+			for(i=0;i<size;i++)
+				mOutputRev[size-1-i]=mOutput[i];
+
+			mOutput[0] = a0*mOutputRev[0];
+			mOutput[1] = a0*mOutputRev[1]+ a1*mOutputRev[0]- b1*mOutput[0];
+			mOutput[2] = a0*mOutputRev[2]+ a1*mOutputRev[1] + a2*mOutputRev[0] - b1*mOutput[1] - b2*mOutput[0];
+			mOutput[3] = a0*mOutputRev[3]+ a1*mOutputRev[2] + a2*mOutputRev[1] + a3*mOutputRev[0] 
+				- b1*mOutput[2] - b2*mOutput[1] - b3*mOutput[0];
+
 			//filtering the reverse signal for 0 phase distortion
-			for(i=0; i<size; i++)
+			for(i=4; i<size; i++)
 			{
-				if(i==0) mOutput[i] = a0*mOutputRev[i];
-				else if(i==1) mOutput[i] = a0*mOutputRev[i]+ a1*mOutputRev[i-1]- b1*mOutput[i-1];
-				else if(i==2) mOutput[i] = a0*mOutputRev[i]+ a1*mOutputRev[i-1] + a2*mOutputRev[i-2] - b1*mOutput[i-1] - b2*mOutput[i-2];
-				else if(i==3) mOutput[i] = a0*mOutputRev[i]+ a1*mOutputRev[i-1] + a2*mOutputRev[i-2] + a3*mOutputRev[i-3] - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3];
-				else mOutput[i] = a0*mOutputRev[i] + a1*mOutputRev[i-1] + a2*mOutputRev[i-2] + a3*mOutputRev[i-3] + a4*mOutputRev[i-4] 
-					     - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4];
-				
+
+				mOutput[i] = a0*mOutputRev[i] + a1*mOutputRev[i-1] + a2*mOutputRev[i-2] + a3*mOutputRev[i-3] + a4*mOutputRev[i-4] 
+					- b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4];				
 			
 			}
 
@@ -221,96 +232,8 @@ namespace CLAM
 
 		}
 
-		void AudioDecimator::Convolute22050SignalWithLowPassEllipticFilter( const DataArray& signal,
-								    DataArray& filteredSignal )
-		{
-			TSize size = signal.Size();
-			int i;
-
-			double a0, a1, a2,a3, a4, a5, a6, a7, a8, b0, b1, b2, b3, b4, b5, b6, b7, b8;
-
-			if ( mOutput.Size() < signal.Size() )
-			{
-				mOutput.Resize( signal.Size() );
-			}								
-
-			mOutput.SetSize( signal.Size() );
-
-			a0 = 0.00219187924814;
-			a1 = -0.00924853933802;
-			a2 = 0.02076893250455;
-			a3 = -0.03100961182809;
-			a4 = 0.03516942453134;
-			a5 = -0.03100961182809;
-			a6 = 0.02076893250455;
-			a7 = -0.00924853933802;
-			a8 = 0.00219187924814;
-			
-			b1 = -6.73202563634183;
-			b2 = 20.52589875623715;
-			b3 = -36.90124484257926;
-			b4 = 42.70393376784493;
-			b5 = -32.53984062505747;
-			b6 = 15.93618579254304;
-			b7 = -4.58665365495816;
-			b8 = 0.59439131759859;	
-			
-						
-			//filtering
-			for(i=0; i<size; i++)
-			{
-				if(i==0) mOutput[i] = a0*signal[i];
-				else if(i==1) mOutput[i] = a0*signal[i]+ a1*signal[i-1]- b1*mOutput[i-1];
-				else if(i==2) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] - b1*mOutput[i-1] - b2*mOutput[i-2];
-				else if(i==3) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3] - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3];
-				else if(i==4) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3]  + a4*signal[i-4]
-						      - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4];
-				else if(i==5) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3]  + a4*signal[i-4]+ a5*signal[i-5]
-						      - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4]- b5*mOutput[i-5];
-				else if(i==6) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3]  + a4*signal[i-4]+ a5*signal[i-5]+ a6*signal[i-6]
-						      - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4]- b5*mOutput[i-5]- b6*mOutput[i-6];
-				else if(i==7) mOutput[i] = a0*signal[i]+ a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3]  + a4*signal[i-4]+ a5*signal[i-5]+ a6*signal[i-6]+ a7*signal[i-7]
-						      - b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4]- b5*mOutput[i-5]- b6*mOutput[i-6]- b7*mOutput[i-7];
-				else mOutput[i] = a0*signal[i] + a1*signal[i-1] + a2*signal[i-2] + a3*signal[i-3] + a4*signal[i-4] + a5*signal[i-5]
-					     + a6*signal[i-6] + a7*signal[i-7] + a8*signal[i-8]	- b1*mOutput[i-1] - b2*mOutput[i-2] - b3*mOutput[i-3] - b4*mOutput[i-4] - b5*mOutput[i-5] - b6*mOutput[i-6] - b7*mOutput[i-7] - b8*mOutput[i-8];
-			}
-			
-
-			// :TODO: This seemed to be a decimation, but
-			// for some unknown reason it is no longer ?????
-			// Ask Fabien and Xavi about this - pretty strange.
-			/*
-			  TSize decimatedSize= size/5;
-			  decimatedSignal.Resize(decimatedSize);
-			  decimatedSignal.SetSize(decimatedSize);
-			  
-			  i=0;
-			  
-			  //decimation
-			  while(i<decimatedSize)
-			  {
-			  decimatedSignal[i] = mOutput[i*5];
-			  i++;
-			  }
-			*/	
-			
-			i=0;
-			
-			while(i<size)
-			{
-				filteredSignal[i] = mOutput[i];
-				i++;
-			}
-
-		}
-	
 		bool AudioDecimator::Do(DataArray &input, int sampleRate, int type)
 		{
-			TSize size = input.Size();
-			int i;
-
-			double a0, a1, a2,a3, a4, a5, a6, a7, a8, b0, b1, b2, b3, b4, b5, b6, b7, b8;
-	
 			switch(type){
 		
 			case 1:
@@ -328,13 +251,6 @@ namespace CLAM
 
 				break;
 
-			case 3:
-				CLAM_ASSERT( sampleRate == 22050, "Sample rate should be 22050Hz!" );
-			
-				Convolute22050SignalWithLowPassEllipticFilter( input, input );
-
-				break;
-		
 			default:
 				CLAM_ASSERT( false, "This conversion is not implemented...");
 			}
