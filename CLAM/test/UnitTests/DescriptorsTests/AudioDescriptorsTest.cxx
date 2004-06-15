1ca5a983db3c5c6ab60dba36aef0c35b47a2b607
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-06-15 17:55:14 +0000

    NaN robustness
    git-svn-id: https://clam-project.org/clam/branches/development-branch@5063 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/test/UnitTests/DescriptorsTests/AudioDescriptorsTest.cxx
+++ CLAM/test/UnitTests/DescriptorsTests/AudioDescriptorsTest.cxx
@@ -119,7 +119,8 @@ private:
 			mDescriptors->SetpAudio(&audio);
 			mDescriptors->Compute();
 			if (
-				std::isnan((mDescriptors->*getter)()) ||
+				(std::isnan((mDescriptors->*getter)()) && !std::isnan(it->second)) ||
+				(!std::isnan((mDescriptors->*getter)()) && std::isnan(it->second)) ||
 				(mDescriptors->*getter)() > (*it).second + tolerance ||
 				(mDescriptors->*getter)() < (*it).second - tolerance
 				)
@@ -191,7 +192,7 @@ private:
 		data["gamelan-gong.wav"] = 1.803790;
 		data["gt_E4.wav"] = 0.646689;
 		data["pno_Eb1.wav"] = 2.341217;
-		data["silence.wav"] = 0.000000;
+		data["silence.wav"] = 0.000000;  // std::numeric_limits<CLAM::TData>::quiet_NaN();
 		data["vln_A3.wav"] = 0.500487;
 		data["vln_D5.wav"] = 0.456816;
 		data["whitenoise.wav"] = 0.049801;
