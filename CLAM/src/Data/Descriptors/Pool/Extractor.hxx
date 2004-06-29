216f544df521d55167ecc39f7f19af6e45d41be1
dgarcia <dgarcia@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-06-29 16:14:16 +0000

    Encapsulate current and iterationSize
    git-svn-id: https://clam-project.org/clam/branches/development-branch@5156 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Data/Descriptors/Pool/Extractor.hxx
+++ CLAM/src/Data/Descriptors/Pool/Extractor.hxx
@@ -11,6 +11,7 @@ template <typename AttributeType>
 class Hook 
 {
 public:
+	virtual ~Hook(){}
 	void Bind(
 			const std::string & scope,
 			const std::string & attribute)
@@ -26,14 +27,14 @@ public:
 
 	bool IsInsideScope() const
 	{
-		return _current < _scopeSize;
+		return _current < GetIterationSize();
 	}
-
 protected:
+	virtual unsigned GetIterationSize() const =0;
+
 	std::string _attribute;
 	std::string _scope;
 	unsigned _current;
-	unsigned _scopeSize;
 };
 
 template <typename AttributeType>
@@ -45,14 +46,24 @@ public:
 		_pool = &pool;
 		_current = 0;
 		_data = _pool->template GetReadAttributePool<AttributeType>(_scope,_attribute);
-		_scopeSize = _pool->GetNumberOfContexts(_scope);
 	}
 
 	const AttributeType & GetForReading() const
 	{
-		return _data [_current];
+		return _data [GetCurrent()];
 	}
+
+protected:
+	virtual unsigned GetIterationSize() const
+	{
+		return _pool->GetNumberOfContexts(_scope);
+	}
+
 private:
+	unsigned GetCurrent() const
+	{
+		return _current;
+	}
 	const DescriptionDataPool * _pool;
 	const AttributeType * _data;
 };
@@ -76,19 +87,27 @@ public:
 		_current = 0;
 		_data = _pool->template GetReadAttributePool<AttributeType>(_scope,_attribute);
 		_indirections = _pool->template GetReadAttributePool<unsigned>(_indirectionScope,_indirectionAttribute);
-		_scopeSize = _pool->GetNumberOfContexts(_indirectionScope);
 		_targetScopeSize = _pool->GetNumberOfContexts(_scope);
 	}
 
 	const AttributeType & GetForReading() const
 	{
+		return _data[GetCurrent()];
+	}
+protected:
+	virtual unsigned GetIterationSize() const
+	{
+		return _pool->GetNumberOfContexts(_indirectionScope);
+	}
+private:
+	unsigned GetCurrent() const
+	{
 		unsigned indirection = _indirections[_current];
 		CLAM_ASSERT(indirection<_targetScopeSize,
 			"Invalid cross-scope reference");
-		return _data[indirection];
+		return indirection;
 	}
 	
-private:
 	const DescriptionDataPool * _pool;
 	std::string _indirectionScope;
 	std::string _indirectionAttribute;
@@ -107,15 +126,23 @@ public:
 		_pool = &pool;
 		_current = 0;
 		_data = _pool->template GetAttributePool<AttributeType>(_scope,_attribute);
-		_scopeSize = _pool->GetNumberOfContexts(_scope);
 	}
 
 	AttributeType & GetForWriting() const
 	{
-		return _data [_current];
+		return _data [GetCurrent()];
 	}
 
+protected:
+	virtual unsigned GetIterationSize() const
+	{
+		return _pool->GetNumberOfContexts(_scope);
+	}
 private:
+	unsigned GetCurrent() const
+	{
+		return _current;
+	}
 	DescriptionDataPool * _pool;
 	AttributeType * _data;
 };
