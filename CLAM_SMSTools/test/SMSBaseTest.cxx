ba10a527f225e4d38f4d4c42b1c7ba35762c4616
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-07-13 17:43:03 +0000

    new test
    git-svn-id: https://clam-project.org/clam/branches/development-branch@5220 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM_SMSTools/test/SMSBaseTest.cxx
+++ CLAM_SMSTools/test/SMSBaseTest.cxx
@@ -35,13 +35,14 @@ class SMSExampleTest : public CppUnit::TestFixture
 	CPPUNIT_TEST( testLoadInputSound_WithAnExistingSoundFile );
 	CPPUNIT_TEST( testLoadInputSound_CalledMultipleTimes );
 	CPPUNIT_TEST( testhelperLoadAudioFromFile );
+	CPPUNIT_TEST( testAnalysis_WithBadAudioInFileName );
 
 	CPPUNIT_TEST( testAnalysisSynthesis_WithDefaultConfig_UsingSine_Wav );
 	CPPUNIT_TEST( testAnalysisSynthesis_WithLoadedConfig_UsingSweep_Wav );
 	CPPUNIT_TEST( testAnalysisSynthesis_WithLoadedConfig_UsingElvis_Wav );
 	CPPUNIT_TEST( testTwoSimpleTransformations_withLoadedScore );
 	CPPUNIT_TEST( testTransformations_withLoadedScore_HarmonizerTimestreach );
-//	CPPUNIT_TEST( testTransformations_withLoadedScore_TimestreachMorph );
+	CPPUNIT_TEST( testTransformations_withLoadedScore_TimestreachMorph );
 	CPPUNIT_TEST_SUITE_END();
 
 
@@ -273,7 +274,23 @@ private:
 		CPPUNIT_ASSERT_MESSAGE( diagnosticSynth, synthAudiosAreEqual );
 	}
 
-
+	void testAnalysis_WithBadAudioInFileName()
+	{
+		mSms->LoadConfig( mPath + "/SMSTests/badConfig.xml" );
+		mSms->InitConfigs();
+		mSms->LoadInputSound();
+		try{
+			mSms->Analyze();
+			CPPUNIT_FAIL("a failed assertion was expected");
+		}
+		catch(CLAM::ErrAssertionFailed& e)
+		{
+			CPPUNIT_ASSERT_EQUAL(
+					std::string("Bad file-name in configuration"), 
+					std::string(e.what()) );
+		}
+		
+	}
 	//-------------------------------------------------------------------------
 
 	void testAnalysisSynthesis_WithDefaultConfig_UsingSine_Wav()
