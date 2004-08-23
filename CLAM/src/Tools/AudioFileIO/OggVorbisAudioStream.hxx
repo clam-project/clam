13e3add7bffc4b22cffe89af67d3e293f49bf368
mramirez <mramirez@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-08-23 17:21:28 +0000

    + Hack removed
    git-svn-id: https://clam-project.org/clam/branches/development-branch@5337 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Tools/AudioFileIO/OggVorbisAudioStream.hxx
+++ CLAM/src/Tools/AudioFileIO/OggVorbisAudioStream.hxx
@@ -4,7 +4,7 @@
 #include <string>
 #include <vorbis/vorbisfile.h>
 #include <vorbis/vorbisenc.h>
-#include "CLAM_deque.hxx"
+#include <deque>
 #include <vector>
 #include "AudioCodecs_Stream.hxx"
 #include "DataTypes.hxx"
