87ad698abe60ba4f4df159d9df862e2007dbef5b
parumi <parumi@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2004-09-20 17:47:34 +0000

    small format thing
    git-svn-id: https://clam-project.org/clam/branches/development-branch@5402 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/src/Processing/AudioFileIO/MultiChannelAudioFileReader.hxx
+++ CLAM/src/Processing/AudioFileIO/MultiChannelAudioFileReader.hxx
@@ -32,8 +32,7 @@ namespace CLAM
 
 	class AudioOutPort;
 	
-	class MultiChannelAudioFileReader 
-		: public Processing
+	class MultiChannelAudioFileReader : public Processing
 	{
 	public:
 		MultiChannelAudioFileReader();
