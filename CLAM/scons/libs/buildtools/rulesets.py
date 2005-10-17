7e4bde0b590ca06c1e18f1dae612252b21330828
uid39306 <uid39306@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2005-10-17 11:59:32 +0000

    + posix rules finished
    git-svn-id: https://clam-project.org/clam/branches/CLAM-with-new-header-style-branch@7634 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

--- CLAM/scons/libs/buildtools/rulesets.py
+++ CLAM/scons/libs/buildtools/rulesets.py
@@ -6,13 +6,13 @@ def handle_preinclude ( env ):
 	return
 
 def posix_lib_rules( name, version, headers, source_files, install_dirs, core_env) :
-	lib_descriptor = core_env.File( 'clam_core.pc' )
+	lib_descriptor = core_env.File( 'clam_'+name+'.pc' )
 
-	soname = 'libclam_core.so.%s'%version.split('.')[0]
-	linker_name = 'libclam_core.so'
+	soname = 'libclam_'+name+'.so.%s'%version.split('.')[0]
+	linker_name = 'libclam_'+name+'.so'
 	core_env.Append(SHLINKFLAGS=['-Wl,-soname,%s'%soname ] )
 
-	lib = core_env.SharedLibrary( 'clam_core', source_files, SHLIBSUFFIX='.so.%s'%version )
+	lib = core_env.SharedLibrary( 'clam_' + name, source_files, SHLIBSUFFIX='.so.%s'%version )
 
 	soname_lib = core_env.SonameLink( soname, lib )
 	linkername_lib = core_env.LinkerNameLink( linker_name, soname_lib)
@@ -22,21 +22,22 @@ def posix_lib_rules( name, version, headers, source_files, install_dirs, core_en
 	install_headers = core_env.Install( install_dirs.inc+'/CLAM', headers )
 	core_env.AddPostAction( install_headers, "chmod 644 $TARGET" )
 
-	install_lib = core_env.Install( install_dirs.lib, linkername_lib )
+	install_lib = core_env.Install( install_dirs.lib, lib)
 	core_env.AddPostAction( install_lib, Action(make_lib_names, make_lib_names_message ) )
 
 	install_descriptor = core_env.Install( install_dirs.lib+'/pkgconfig', lib_descriptor )
 
-	core_install_tgt = core_env.Alias( 'install_core', [install_headers, install_lib, install_descriptor] )
+	core_install_tgt = core_env.Alias( 'install_' + name, [install_headers, install_lib, install_descriptor] )
 
-	runtime_lib = core_env.Install( install_dirs.lib, soname_lib )
+	runtime_lib = core_env.Install( install_dirs.lib, lib )
+	runtime_soname = core_env.SonameLink( install_dirs.lib + '/' + soname, runtime_lib )
 
-	core_env.Alias( 'install_core_runtime', soname_lib )
+	core_env.Alias( 'install_'+name+'_runtime', [runtime_lib, runtime_soname] )
 
-	static_lib = core_env.Library( 'clam_core', source_files )
+	static_lib = core_env.Library( 'clam_'+name, source_files )
 	install_static = core_env.Install( install_dirs.lib, static_lib )
 
 	dev_linkername =  core_env.LinkerNameLink( install_dirs.lib+'/'+linker_name, install_dirs.lib+'/'+soname) 
-	core_env.Alias( 'install_core_dev', [install_headers,dev_linkername, install_descriptor, install_static] )
-
+	core_env.Alias( 'install_'+name+'_dev', [install_headers,dev_linkername, install_descriptor, install_static] )
+	
 	return core_tgt, core_install_tgt
