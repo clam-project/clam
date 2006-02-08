a449e6518524b97cbb1c8b3ce4cb5d8809c260f1
(no author) <(no author)@1f510bfa-ff1f-0410-ab8f-dacffcba9fb9>
2006-02-08 13:20:02 +0000

    This commit was manufactured by cvs2svn to create tag 'CLAM-0_90_0-rel'.
    git-svn-id: https://clam-project.org/clam/tags/CLAM-0_90_0-rel@8316 1f510bfa-ff1f-0410-ab8f-dacffcba9fb9

---
+++ CLAM/src/Visualization/QTVM/Network/Renderers/BuffDataRenderer.hxx
@@ -0,0 +1,31 @@
+#ifndef __BUFFDATARENDERER__
+#define __BUFFDATARENDERER__
+
+#include "Renderer.hxx"
+
+namespace CLAM
+{
+    namespace VM
+    {
+		class BuffDataRenderer : public Renderer
+		{
+		public:
+			BuffDataRenderer();
+			~BuffDataRenderer();
+
+			void SetDataPtr(const TData* data);
+			void SetIndexes(const TIndex& left1, const TIndex& right1, 
+							const TIndex& left2, const TIndex& right2, 
+							const TIndex& begin);
+			void Render();
+
+		private:
+			const TData* mData;
+			TIndex mLeftIndex1, mRightIndex1, mLeftIndex2, mRightIndex2;
+			TIndex mBegin;
+		};
+    }
+}
+
+#endif
+
